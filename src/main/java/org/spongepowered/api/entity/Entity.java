a35df993ebc766f76106a8bdd7a5382c89adf055
Lennart ten Wolde <0p1q9o2w@hotmail.nl>
2014-09-09 08:43:25 +0200

    Add Entity
    remove public from methods
    
    Add Entity

--- src/main/java/org/spongepowered/api/entity/Entity.java
+++ src/main/java/org/spongepowered/api/entity/Entity.java
@@ -28,10 +28,203 @@ import java.util.UUID;
 public interface Entity {
 
     /**
-     * Gets the unique ID for this entity
+     * Get id of the entity.
+     * Used for communication with the client.
      *
-     * @return The entity's {@link UUID}
+     * @return Id of entity
+     */
+    int getID();
+
+    /**
+     * Get unique id of entity
+     * Used for identifying the entity.
+     *
+     * @return Unique entity id
      */
     UUID getUniqueID();
 
+    /**
+     * Check if the entity is standing on a solid block.
+     *
+     * @return Entity on ground?
+     */
+    boolean isOnGround();
+
+    /**
+     * Check if the entity is alive.
+     *
+     * @return Entity alive?
+     */
+    boolean isAlive();
+
+    /**
+     * Check if the entity is dead.
+     *
+     * @return Entity dead?
+     */
+    boolean isDead();
+
+    /**
+     * Get height of the entity in blocks/meters.
+     *
+     * @return Height of entity
+     */
+    double getHeight();
+
+    /**
+     * Get width of the entity in blocks/meters.
+     *
+     * @return Width of entity
+     */
+    double getWidth();
+
+    /**
+     * Get length of the entity in blocks/meters.
+     *
+     * @return Length of entity
+     */
+    double getLength();
+
+    /**
+     * Get the current distance the entity has been falling for.
+     *
+     * @return Entity falling distance
+     */
+    float getFallDistance();
+
+    /**
+     * Set the currenct distance the entity has been falling for.
+     *
+     * @param fallDistance Entity falling distance
+     */
+    void setFallDistance(float fallDistance);
+
+    /**
+     * Get the amount of ticks the entity has lived for.
+     * Ex. 40 (= 2 seconds)
+     *
+     * @return Entity alive ticks
+     */
+    int getTicksLived();
+
+    /**
+     * Set the amount of ticks the entity has lived for.
+     * Ex. 40 (= 2 seconds)
+     *
+     * @param ticksLived Entity alive ticks
+     */
+    void setTicksLived(int ticksLived);
+
+    /**
+     * Get the amount of ticks the entity will be put on fire for when interacting with lava or fire.
+     * Ex. 40 (= 2 seconds)
+     *
+     * @return Entity max fire ticks.
+     */
+    int getMaxFireTicks();
+
+    /**
+     * Get the remaining ticks the entity is on fire for.
+     *
+     * @return Entity fire ticks
+     */
+    int getFireTicks();
+
+    /**
+     * Set the remaining ticks the entity is on fire for.
+     *
+     * @param fireTicks Entity fire ticks
+     */
+    void setFireTicks(int fireTicks);
+
+    /**
+     * Check if the entity is in water.
+     *
+     * @return Entity in water?
+     */
+    boolean isInWater();
+
+    /**
+     * Get the amount of ticks the entity will not be damaged for.
+     *
+     * @return Entity no-damage ticks
+     */
+    int getNoDamageTicks();
+
+    /**
+     * Set the amount of ticks the entity will not be damaged for.
+     *
+     * @param noDamageTicks Entity no-damage ticks
+     */
+    void setNoDamageTicks(int noDamageTicks);
+
+    /**
+     * Check if the entity is invulnerable to fire.
+     *
+     * @return Entity invulnerable to fire?
+     */
+    boolean isFireProof();
+
+    /**
+     * Set if the entity should be invulnerable to fire.
+     *
+     * @param fireProof Entity invulnerable to fire
+     */
+    void setFireProof(boolean fireProof);
+
+    /**
+     * Get the amount of ticks the entity is ignored by portal blocks
+     *
+     * @return Portal cooldown in ticks
+     */
+    int getPortalCooldown();
+
+    /**
+     * Set the amount of ticks the entity is ignored by portal blocks
+     *
+     * @param portalCooldown Portal cooldown in ticks
+     */
+    void setPortalCooldown(int portalCooldown);
+
+    /**
+     * Check if the entity is invulnerable to all damage.
+     *
+     * @return Enity invulnerable?
+     */
+    boolean isInvulnerable();
+
+    /**
+     * Set if the entity is invulnerable to all damage.
+     *
+     * @param invulnerable Entity invulnerable
+     */
+    void setInvulnerable(boolean invulnerable);
+
+    /**
+     * Teleport the entity to another entity.
+     *
+     * @param entity Entity to teleport to
+     */
+    void teleport(Entity entity);
+
+    /**
+     * Get the entity that has this entity as passenger.
+     *
+     * @return Vehicle if the entity is inside of a vehicle, otherwise null.
+     */
+    Entity getVehicle();
+
+    /**
+     * Check if the entity is inside of a vehicle.
+     *
+     * @return Entity inside vehicle?
+     */
+    boolean isInsideVehicle();
+
+    /**
+     * Set an entity to be passenger of this entity.
+     *
+     * @param entity Entity to be used as passenger.
+     */
+    void setPassenger(Entity entity);
 }
