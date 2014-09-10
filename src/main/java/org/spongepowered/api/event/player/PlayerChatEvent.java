e61b60cd47032b8c20ce2d3ce0aaea619e4ee171
Dylan Katz <dylanishappy1@gmail.com>
2014-09-09 20:55:14 -0600

    Add PlayerChat and PlayerChatEvent
    Generic class for player events and class for chat events.

---
+++ src/main/java/org/spongepowered/api/event/player/PlayerChatEvent.java
@@ -0,0 +1,58 @@
+/**
+ * This file is part of SpongeAPI, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2014 SpongePowered <http://spongepowered.org/>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.api.event.player;
+
+import org.spongepowered.api.Game;
+import org.spongepowered.api.entity.Player;
+
+/**
+ * Called when a {@link Player} sends a chat message
+ *
+ */
+public class PlayerChatEvent extends PlayerEvent {
+    public final String message;
+    private Game game;
+    /**
+     * Called when a {@link Player} sends a chat message
+     *
+     * @param game Game
+     * @param sender The sender of the chat message
+     * @param message The message sending
+     */
+    public PlayerChatEvent(Game game, Player sender, String message) {
+        super(sender);
+        this.game = game;
+        this.message = message;
+    }
+
+    @Override
+    public Game getGame() {
+        return game;
+    }
+
+    @Override
+    public String getSimpleName() {
+        return "PlayerChat";
+    }
+}
