678315c13719d333ecb6ec1e66f7bc77a6faba51
Theodoros Ntakouris <zarkopafilis@gmail.com>
2014-09-12 14:49:17 +0300

    Update PlayerBlockPlaceEvent.java

--- src/main/java/org/spongepowered/api/event/player/PlayerBlockPlaceEvent.java
+++ src/main/java/org/spongepowered/api/event/player/PlayerBlockPlaceEvent.java
@@ -23,7 +23,7 @@
  */
 package org.spongepowered.api.event.player;
 
-import org.spongepowered.api.event.PlayerEvent;
+import org.spongepowered.api.event.player.PlayerEvent;
 
 /**
  * Player tries to login server , first event triggered
