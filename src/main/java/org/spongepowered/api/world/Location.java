96f26c65f546abc924d38d7f8e1d15bcd0b218c7
AlexTheCoder <groupmoderatorforever@gmail.com>
2014-09-12 01:01:56 -0400

    Added a location class and AsyncPlayerMoveEvent
    Added location and AsyncPlayerMoveEvent with getTo() and getFrom()

---
+++ src/main/java/org/spongepowered/api/world/Location.java
@@ -0,0 +1,71 @@
+/**
+ * This file is part of SpongeAPI, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2014 SpongePowered <http://spongepowered.org/>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.api.world;
+
+public class Location implements Cloneable{
+	
+	private World world;
+	private double x;
+	private double y;
+	private double z;
+	
+	public Location(World world, double x, double y, double z) {
+	    this.world = world;
+	    this.x = x;
+	    this.y = y;
+	    this.z = z;
+	}
+	
+	public void setWorld(World w) {
+		this.world = w;
+	}
+	
+	public World getWorld() {
+		return this.world;
+	}
+	
+	public void setX(double x) {
+		this.x = x;
+	}
+	
+	public double getX() {
+		return this.x;
+	}
+	
+	public void setY(double y) {
+		this.y = y;
+	}
+	
+	public double getY() {
+		return this.y;
+	}
+	public void setZ(double z) {
+		this.z = z;
+	}
+	
+	public double getZ() {
+		return this.z;
+	}
+
+}
