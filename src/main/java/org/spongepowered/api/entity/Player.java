5daa6867808531e587916ddcce274b80d9d3f75d
Maxim Van de Wynckel <maxim_vdw@hotmail.com>
2014-09-12 07:33:42 +0200

    Player online state
    Check if a player is online or not
    Currently we don't know if there will be a class for an offlline player, but yes or no its always
    handy to know if a player instance is online or not

--- src/main/java/org/spongepowered/api/entity/Player.java
+++ src/main/java/org/spongepowered/api/entity/Player.java
@@ -39,4 +39,12 @@ public interface Player extends HumanEntity {
      * @return The player's display name
      */
     String getDisplayName();
+    
+    /**
+     * Gets the player's online state.
+     * returns if the player is online or not.
+     *
+     * @return The player's online state
+     */
+    boolean isOnline();
 }
