3dde72e2aaf661017df165963b55440e2523e816
SmooshCakez <SmooshCakez@gmail.com>
2014-09-13 12:07:28 -0400

    Update AsyncPlayerChatEvent.java

--- src/main/java/org/spongepowered/api/event/player/AsyncPlayerChatEvent.java
+++ src/main/java/org/spongepowered/api/event/player/AsyncPlayerChatEvent.java
@@ -24,6 +24,7 @@
 package org.spongepowered.api.event.player;
 
 import org.spongepowered.api.entity.Player;
+import java.util.Collection;
 
 /**
  * Called when a {@link Player} sends a chat message
