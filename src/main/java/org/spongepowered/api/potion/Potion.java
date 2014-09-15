3791dfdc8187b491f185c950caf3bede5ba29e2d
baseball435 <jmrapp1@comcast.net>
2014-09-15 10:56:09 -0700

    Potion Apply Method Addition

--- src/main/java/org/spongepowered/api/potion/Potion.java
+++ src/main/java/org/spongepowered/api/potion/Potion.java
@@ -1,8 +1,22 @@
 package org.spongepowered.api.potion;
 
+import org.spongepowered.api.entity.Entity;
+
 public interface Potion {
 
+	/**
+	 * Gets the type of potion.
+	 * 
+	 * @return PotionType The PotionType.
+	 */
 	public PotionType getType();
+	
+	/**
+	 * Applies the potion effects to the entity.
+	 * 
+	 * @param entity The entity to apply the effects to.
+	 */
+	public void apply(Entity entity);
 	
 	/** 
 	 * Gets the amount of time the potion has to affect the entity.
