9d929662e6a0295136bb82747a6cbf321bf5ed48
Progwml6 <progwml6@gmail.com>
2014-09-15 23:19:05 -0400

    update example plugin for new logger changes

--- src/main/example/main/java/org/spongepowered/example/ExamplePlugin.java
+++ src/main/example/main/java/org/spongepowered/example/ExamplePlugin.java
@@ -1,26 +1,30 @@
 package org.spongepowered.example;
 
+import org.apache.logging.log4j.Logger;
 import org.spongepowered.api.event.SpongeEventHandler;
+import org.spongepowered.api.event.state.ConstructionEvent;
 import org.spongepowered.api.event.state.InitializationEvent;
+import org.spongepowered.api.event.state.PreInitializationEvent;
 import org.spongepowered.api.event.state.ServerStartingEvent;
 import org.spongepowered.api.event.voxel.VoxelEvent;
 import org.spongepowered.api.plugin.Plugin;
 
 @Plugin(id = "ExamplePlugin", name = "Example")
 public class ExamplePlugin {
-
+    Logger logger;
     @SpongeEventHandler
-    public void onInitialization(InitializationEvent event) {
-        event.getGame().getLogger().info("Hey folks, this is INITIALIZATION!");
+    public void onConstruction(PreInitializationEvent event) {
+        logger = event.getPluginLog();
+        logger.info("Hey folks, this is PRE-INITIALIZATION!");
     }
 
     @SpongeEventHandler
     public void onServerStarting(ServerStartingEvent event) {
-        event.getGame().getLogger().info("Hey...my implementation's server is starting?");
+        logger.info("Hey...my implementation's server is starting?");
     }
     
     @SpongeEventHandler
     public void onVoxelEvent(VoxelEvent event) {
-        event.getGame().getLogger().info("A Block was broken");
+        logger.info("A Block was broken");
     }
 }
