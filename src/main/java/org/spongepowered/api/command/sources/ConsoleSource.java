8edb39baf98042ef5e37489ef0ec2a9dec981796
baseball435 <jmrapp1@comcast.net>
2014-09-16 22:42:51 -0700

    Added Command Sources

---
+++ src/main/java/org/spongepowered/api/command/sources/ConsoleSource.java
@@ -0,0 +1,7 @@
+package org.spongepowered.api.command.sources;
+
+import org.spongepowered.api.command.CommandSource;
+
+public class ConsoleSource implements CommandSource {
+
+}
---
+++ src/main/java/org/spongepowered/api/command/sources/RconSource.java
@@ -0,0 +1,7 @@
+package org.spongepowered.api.command.sources;
+
+import org.spongepowered.api.command.CommandSource;
+
+public class RconSource implements CommandSource {
+
+}
