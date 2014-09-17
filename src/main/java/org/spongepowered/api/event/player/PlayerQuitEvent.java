7c2b1549a13618aca11a9fc39988c7d512a560e8
Lingo93 <dkatz@mgsrv.net>
2014-09-17 13:33:58 +0200

    add PlayerJoinEvent and PlayerQuitEvent

---
+++ src/main/java/org/spongepowered/api/event/player/PlayerQuitEvent.java
@@ -0,0 +1,56 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered.org <http://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package org.spongepowered.api.event.player;
+
+import org.spongepowered.api.entity.Player;
+
+/**
+ * Called when a {@link Player} leave the game
+ */
+public interface PlayerQuitEvent extends PlayerEvent {
+
+    /**
+     * Gets the message that will be broadcast when a player leave
+     *
+     * @return The join message
+     */
+    String getMessage();
+    
+    /**
+	 * Sets the message that will be displayed when the player leave
+	 *
+	 * @param message to be set
+	 */
+    void setMessage(String message);
+    
+    /**
+     * Gets if the player was kicked
+	 *
+	 * @return true if the player was kicked
+     */
+    boolean isKick();
+    
+}
