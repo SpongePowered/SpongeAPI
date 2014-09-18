b881cf5aaa2881e268db010670707029b278a68b
Me4502 <mnmiller1@hotmail.com>
2014-09-18 19:44:03 +1000

    Added Euler Direction Interface

---
+++ src/main/java/org/spongepowered/api/math/EulerDirection.java
@@ -0,0 +1,55 @@
+/**
+ * This file is part of SpongeAPI, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2014 SpongePowered <http://spongepowered.org/>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.api.math;
+
+/**
+ * Represents a euler direction, made up of both a pitch and yaw component.
+ *
+ * Euler directions are most commonly used by entities to represent the direction they are looking in.
+ */
+public interface EulerDirection {
+
+    /**
+     * Returns the pitch component.
+     *
+     * @return the pitch component.
+     */
+    float getPitch();
+
+    /**
+     * Returns the yaw component.
+     *
+     * @return the yaw component.
+     */
+    float getYaw();
+
+    /**
+     * Converts this Euler Direction into a {@link Vector2d}
+     *
+     * The vector will represent the directions that this direction is facing in x,y,z coordinates, with a length of 1.
+     *
+     * @return the vector representation of this direction.
+     */
+    Vector2d toVector();
+}
