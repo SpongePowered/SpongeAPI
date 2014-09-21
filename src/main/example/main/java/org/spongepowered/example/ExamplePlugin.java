da89b2a475865a371af11cc4d7a3085f76792d3d
Raphfrk <raphfrk@gmail.com>
2014-09-21 14:59:13 +0100

    Added base class for custom events

--- src/main/example/main/java/org/spongepowered/example/ExamplePlugin.java
+++ src/main/example/main/java/org/spongepowered/example/ExamplePlugin.java
@@ -1,9 +1,10 @@
 package org.spongepowered.example;
 
 import org.apache.logging.log4j.Logger;
+import org.spongepowered.api.Game;
+import org.spongepowered.api.event.BaseEvent;
+import org.spongepowered.api.event.Event;
 import org.spongepowered.api.event.SpongeEventHandler;
-import org.spongepowered.api.event.state.ConstructionEvent;
-import org.spongepowered.api.event.state.InitializationEvent;
 import org.spongepowered.api.event.state.PreInitializationEvent;
 import org.spongepowered.api.event.state.ServerStartingEvent;
 import org.spongepowered.api.event.voxel.VoxelEvent;
@@ -12,8 +13,11 @@ import org.spongepowered.api.plugin.Plugin;
 @Plugin(id = "ExamplePlugin", name = "Example")
 public class ExamplePlugin {
     Logger logger;
+    Game game;
+    
     @SpongeEventHandler
     public void onConstruction(PreInitializationEvent event) {
+        game = event.getGame();
         logger = event.getPluginLog();
         logger.info("Hey folks, this is PRE-INITIALIZATION!");
     }
@@ -21,10 +25,33 @@ public class ExamplePlugin {
     @SpongeEventHandler
     public void onServerStarting(ServerStartingEvent event) {
         logger.info("Hey...my implementation's server is starting?");
+        
+        logger.info("Sending custom event to EventManager");
+        Event e = new MyEvent("Custom event message");
+        game.getEventManager().call(e);
+    }
+    
+    @SpongeEventHandler
+    public void onMyEvent(MyEvent event) {
+        logger.info("Custom event received (" + event.getMessage() + ")");
     }
     
     @SpongeEventHandler
     public void onVoxelEvent(VoxelEvent event) {
         logger.info("A Block was broken");
+    }
+    
+    public class MyEvent extends BaseEvent {
+        
+        private final String message;
+        
+        public MyEvent(String message) {
+            this.message = message;
+        }
+        
+        public String getMessage() {
+            return message;
+        }
+        
     }
 }
