650b4909f2899850d9e2ce31b62ce9a3a2ac188d
Dylan Katz <dylanishappy1@gmail.com>
2014-10-26 22:22:50 -0600

    Revert "updated license"
    This reverts commit e6b13e747973ec037baacc8471860011d61725e3.

--- src/main/java/org/spongepowered/api/item/ItemBlock.java
+++ src/main/java/org/spongepowered/api/item/ItemBlock.java
@@ -1,8 +1,7 @@
-/*
- * This file is part of Sponge, licensed under the MIT License (MIT).
+/**
+ * This file is part of SpongeAPI, licensed under the MIT License (MIT).
  *
- * Copyright (c) SpongePowered.org <http://www.spongepowered.org>
- * Copyright (c) contributors
+ * Copyright (c) 2014 SpongePowered <http://spongepowered.org/>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
