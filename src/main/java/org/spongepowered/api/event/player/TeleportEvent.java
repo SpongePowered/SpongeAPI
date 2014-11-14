3cf8435441393b0b692dee54a816a95badd9def2
12AwsomeMan34 <aaronlingerfelt@yahoo.com>
2014-11-13 21:17:02 -0500

    Added TeleportEvent for player
    Includes possible reasons for being teleported as a player.

---
+++ src/main/java/org/spongepowered/api/event/player/TeleportEvent.java
@@ -0,0 +1,41 @@
+package org.spongepowered.api.event.player;
+
+public interface TeleportEvent {
+	
+	/**
+	 * Gets the reason for teleportation.
+	 * 
+	 * @return The reason for teleportation.
+	 */
+	TeleportReason getReason();
+	
+	/**
+	 * The possible reasons for a player being teleported.
+	 */
+	public enum TeleportReason {
+		/**
+		 * The player was teleported by an execute of a command.
+		 */
+		COMMAND,
+		
+		/**
+		 * The player was teleported by the entrance of an end portal.
+		 */
+		END_PORTAL,
+		
+		/**
+		 * The player was teleported by the launching of an ender pearl.
+		 */
+		ENDER_PEARL,
+		
+		/**
+		 * The player was teleported by the entrance of an nether portal.
+		 */
+		NETHER_PORTAL,
+		
+		/**
+		 * The player was teleported by an external plugin.
+		 */
+		PLUGIN
+	}
+}
