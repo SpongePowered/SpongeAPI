2e270c57cedab66572ae87a8d73a83dbb0aa435e
12AwsomeMan34 <aaronlingerfelt@yahoo.com>
2014-11-15 09:12:53 -0500

    Basic seperation of class and interface
    Achievement is the interface through wich will be implemented.
    Achievements is the list of achievements in a class as constants.

--- src/main/java/org/spongepowered/api/misc/Achievement.java
+++ src/main/java/org/spongepowered/api/misc/Achievement.java
@@ -1,67 +1,12 @@
-/*
- * This file is part of Sponge, licensed under the MIT License (MIT).
- *
- * Copyright (c) SpongePowered.org <http://www.spongepowered.org>
- * Copyright (c) contributors
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 package org.spongepowered.api.misc;
 
-public class Achievement {
+public interface Achievement {
 	
 	/**
-	 * The list of achievements.
+	 * Gets the list of achievements within vanilla Minecraft.
+	 * 
+	 * @return The list of achievements
 	 */
-	public static final Achievement TAKING_INVENTORY = new Achievement();
-	public static final Achievement GETTING_WOOD = new Achievement();
-	public static final Achievement BENCHMARKING = new Achievement();
-	public static final Achievement TIME_TO_MINE = new Achievement();
-	public static final Achievement HOT_TOPIC = new Achievement();
-	public static final Achievement ACQUIRE_HARDWARE = new Achievement();
-	public static final Achievement TIME_TO_FARM = new Achievement();
-	public static final Achievement BAKE_BREAD = new Achievement();
-	public static final Achievement THE_LIE = new Achievement();
-	public static final Achievement GETTING_AN_UPGRADE = new Achievement();
-	public static final Achievement DELICIOUS_FISH = new Achievement();
-	public static final Achievement ON_A_RAIL = new Achievement();
-	public static final Achievement TIME_TO_STRIKE = new Achievement();
-	public static final Achievement MONSTER_HUNTER = new Achievement();
-	public static final Achievement COW_TIPPER = new Achievement();
-	public static final Achievement WHEN_PIGS_FLY = new Achievement();
-	public static final Achievement SNIPER_DUEL = new Achievement();
-	public static final Achievement DIAMONDS = new Achievement();
-	public static final Achievement WE_NEED_TO_GO_DEEPER = new Achievement();
-	public static final Achievement RETURN_TO_SENDER = new Achievement();
-	public static final Achievement INTO_FIRE = new Achievement();
-	public static final Achievement LOCAL_BREWERY = new Achievement();
-	public static final Achievement THE_END_PORTAL = new Achievement();
-	public static final Achievement THE_END_DRAGON = new Achievement();
-	public static final Achievement ENCHANTER = new Achievement();
-	public static final Achievement OVERKILL = new Achievement();
-	public static final Achievement LIBRARIAN = new Achievement();
-	public static final Achievement ADVENTURING_TIME = new Achievement();
-	public static final Achievement THE_BEGINNING_SPAWN = new Achievement();
-	public static final Achievement THE_BEGINNING_DEFEAT = new Achievement();
-	public static final Achievement BEACONATOR = new Achievement();
-	public static final Achievement REPOPULATION = new Achievement();
-	public static final Achievement DIAMONDS_TO_YOU = new Achievement();
-	public static final Achievement OVERPOWERED = new Achievement();
+	Achievements getAchievements();
+	
 }
---
+++ src/main/java/org/spongepowered/api/misc/Achievements.java
@@ -0,0 +1,68 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered.org <http://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package org.spongepowered.api.misc;
+
+public class Achievements {
+	
+	/**
+	 * The list of achievements within vanilla Minecraft.
+	 */
+	public static final Achievements TAKING_INVENTORY = null;
+	public static final Achievements GETTING_WOOD = null;
+	public static final Achievements BENCHMARKING = null;
+	public static final Achievements TIME_TO_MINE = null;
+	public static final Achievements HOT_TOPIC = null;
+	public static final Achievements ACQUIRE_HARDWARE = null;
+	public static final Achievements TIME_TO_FARM = null;
+	public static final Achievements BAKE_BREAD = null;
+	public static final Achievements THE_LIE = null;
+	public static final Achievements GETTING_AN_UPGRADE = null;
+	public static final Achievements DELICIOUS_FISH = null;
+	public static final Achievements ON_A_RAIL = null;
+	public static final Achievements TIME_TO_STRIKE = null;
+	public static final Achievements MONSTER_HUNTER = null;
+	public static final Achievements COW_TIPPER = null;
+	public static final Achievements WHEN_PIGS_FLY = null;
+	public static final Achievements SNIPER_DUEL = null;
+	public static final Achievements DIAMONDS = null;
+	public static final Achievements WE_NEED_TO_GO_DEEPER = null;
+	public static final Achievements RETURN_TO_SENDER = null;
+	public static final Achievements INTO_FIRE = null;
+	public static final Achievements LOCAL_BREWERY = null;
+	public static final Achievements THE_END_PORTAL = null;
+	public static final Achievements THE_END_DRAGON = null;
+	public static final Achievements ENCHANTER = null;
+	public static final Achievements OVERKILL = null;
+	public static final Achievements LIBRARIAN = null;
+	public static final Achievements ADVENTURING_TIME = null;
+	public static final Achievements THE_BEGINNING_SPAWN = null;
+	public static final Achievements THE_BEGINNING_DEFEAT = null;
+	public static final Achievements BEACONATOR = null;
+	public static final Achievements REPOPULATION = null;
+	public static final Achievements DIAMONDS_TO_YOU = null;
+	public static final Achievements OVERPOWERED = null;
+	
+}
