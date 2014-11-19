2a79341154317d8a3d9e583a5d8b922bcb39fd21
gratimax <max@ovsankin.com>
2014-11-19 12:05:11 -0800

    Begin stats package

---
+++ src/main/java/org/spongepowered/api/stats/Statistic.java
@@ -0,0 +1,15 @@
+package org.spongepowered.api.stats;
+
+/**
+ * Represents some statistic in Minecraft with a string ID.
+ */
+public interface Statistic {
+
+    /**
+     * Gets the ID for this Statistic.
+     *
+     * @return The String ID
+     */
+    String getId();
+
+}
