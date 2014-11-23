ce685ce4df46f0acb365574c92b2a683d182a460
Me4502 <mnmiller1@hotmail.com>
2014-11-23 18:31:50 +1000

    Added many weather events, as well as entitytame and entityteleport.

--- src/main/java/org/spongepowered/api/event/entity/EntityChangeBlockEvent.java
+++ src/main/java/org/spongepowered/api/event/entity/EntityChangeBlockEvent.java
@@ -5,6 +5,8 @@ import org.spongepowered.api.entity.Entity;
 
 /**
  * Called when an {@link Entity} changes a {@link Block}.
+ *
+ * TODO Add extends BlockChangeEvent once merged.
  */
 public interface EntityChangeBlockEvent extends EntityEvent {
 
