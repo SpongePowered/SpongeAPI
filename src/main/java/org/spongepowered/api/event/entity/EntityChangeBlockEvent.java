8b77e7f85843c4e8c779ba853fd3f72477aef680
Me4502 <mnmiller1@hotmail.com>
2014-11-23 17:54:42 +1000

    Added EntityChangeBlockEvent

---
+++ src/main/java/org/spongepowered/api/event/entity/EntityChangeBlockEvent.java
@@ -0,0 +1,11 @@
+package org.spongepowered.api.event.entity;
+
+import org.spongepowered.api.block.Block;
+import org.spongepowered.api.entity.Entity;
+
+/**
+ * Called when an {@link Entity} changes a {@link Block}.
+ */
+public interface EntityChangeBlockEvent extends EntityEvent {
+
+}
