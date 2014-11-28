04663e2e4ed038aa57981abb2569592c015fdfb5
Me4502 <mnmiller1@hotmail.com>
2014-11-28 18:08:30 +1000

    Added basic potion API.

---
+++ src/main/java/org/spongepowered/api/potion/PotionEffectTypes.java
@@ -0,0 +1,31 @@
+package org.spongepowered.api.potion;
+
+/**
+ * An enumaration of all possible {@link PotionEffectType}s in vanilla minecraft.
+ */
+public class PotionEffectTypes {
+
+    public static final PotionEffectType SPEED = null;
+    public static final PotionEffectType SLOWNESS = null;
+    public static final PotionEffectType HASTE = null;
+    public static final PotionEffectType MINING_FATIGUE = null;
+    public static final PotionEffectType STRENGTH = null;
+    public static final PotionEffectType INSTANT_HEALTH = null;
+    public static final PotionEffectType INSTANT_DAMAGE = null;
+    public static final PotionEffectType JUMP_BOOST = null;
+    public static final PotionEffectType NAUSEA = null;
+    public static final PotionEffectType REGENERATION = null;
+    public static final PotionEffectType RESISTANCE = null;
+    public static final PotionEffectType FIRE_RESISTANCE = null;
+    public static final PotionEffectType WATER_BREATHING = null;
+    public static final PotionEffectType INVISIBILITY = null;
+    public static final PotionEffectType BLINDNESS = null;
+    public static final PotionEffectType NIGHT_VISION = null;
+    public static final PotionEffectType HUNGER = null;
+    public static final PotionEffectType WEAKNESS = null;
+    public static final PotionEffectType POISON = null;
+    public static final PotionEffectType WITHER = null;
+    public static final PotionEffectType HEALTH_BOOST = null;
+    public static final PotionEffectType ABSORPTION = null;
+    public static final PotionEffectType SATURATION = null;
+}
