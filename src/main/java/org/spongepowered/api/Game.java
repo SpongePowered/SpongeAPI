0840a75af0a84d671927fd61f2ebd61a43159a3e
Chris Sanders <zidane@outlook.com>
2014-12-02 20:21:51 -0600

    Split Server from Game
    Add API for a few other typical Server attributes
    
    Signed-off-by: Chris Sanders <zidane@outlook.com>

--- src/main/java/org/spongepowered/api/Game.java
+++ src/main/java/org/spongepowered/api/Game.java
@@ -26,18 +26,11 @@
 package org.spongepowered.api;
 
 import com.google.common.base.Optional;
-import org.spongepowered.api.entity.player.Player;
 import org.spongepowered.api.plugin.PluginManager;
 import org.spongepowered.api.service.ServiceManager;
 import org.spongepowered.api.service.command.CommandService;
 import org.spongepowered.api.service.event.EventManager;
 import org.spongepowered.api.service.scheduler.Scheduler;
-import org.spongepowered.api.text.message.Message;
-import org.spongepowered.api.text.title.Title;
-import org.spongepowered.api.world.World;
-
-import java.util.Collection;
-import java.util.UUID;
 
 /**
  * The core accessor of the API. The implementation uses this to pass
@@ -52,6 +45,12 @@ public interface Game {
      * @return The platform
      */
     Platform getPlatform();
+
+    /**
+     * Gets the running {@link Server}, if any.
+     * @return The server
+     */
+    Optional<Server> getServer();
 
     /**
      * Gets the {@link PluginManager}.
@@ -98,71 +97,6 @@ public interface Game {
      * @return The command dispatcher
      */
     CommandService getCommandDispatcher();
-
-    /**
-     * Gets the {@link Player}s currently online
-     *
-     * @return A {@link Collection} of online players
-     */
-    Collection<Player> getOnlinePlayers();
-
-    /**
-     * Gets the max players allowed on this server
-     *
-     * @return Maximum number of connected players
-     */
-    int getMaxPlayers();
-
-    /**
-     * Gets a {@link Player} by their unique id
-     *
-     * @param uniqueId The UUID to get the player from
-     * @return {@link Player} if available
-     */
-    Optional<Player> getPlayer(UUID uniqueId);
-
-    /**
-     * Gets a {@link Player} by their name
-     *
-     * This only works for online players.
-     *
-     * <b>Note: Do not use names for persistent storage, the
-     * Zidane of today may not be the Zidane of yesterday.</b>
-     *
-     * @param name The name to get the player from
-     * @return {@link Player} if available
-     */
-    Optional<Player> getPlayer(String name);
-
-    /**
-     * Gets all currently loaded {@link World}s.
-     *
-     * @return Collection of loaded worlds
-     */
-    Collection<World> getWorlds();
-
-    /**
-     * Gets a loaded {@link World} by its unique id ({@link UUID}).
-     *
-     * @param uniqueId UUID to lookup
-     * @return The world or null if not found
-     */
-    World getWorld(UUID uniqueId);
-
-    /**
-     * Gets a loaded {@link World} by name
-     *
-     * @param worldName Name to lookup
-     * @return The world or null if not found
-     */
-    World getWorld(String worldName);
-
-    /**
-     * Sends the given message to all online players
-     *
-     * @param message The message to send
-     */
-    void broadcastMessage(Message<?> message);
 
     /**
      * Gets the API version.
---
+++ src/main/java/org/spongepowered/api/Server.java
@@ -0,0 +1,138 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered.org <http://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.api;
+
+import com.google.common.base.Optional;
+import org.spongepowered.api.entity.player.Player;
+import org.spongepowered.api.text.message.Message;
+import org.spongepowered.api.world.World;
+
+import java.net.InetSocketAddress;
+import java.util.Collection;
+import java.util.UUID;
+
+/**
+ * Represents a typical Minecraft Server.
+ */
+public interface Server {
+    /**
+     * Gets the {@link Player}s currently online
+     *
+     * @return A {@link Collection} of online players
+     */
+    Collection<Player> getOnlinePlayers();
+
+    /**
+     * Gets the max players allowed on this server
+     *
+     * @return Maximum number of connected players
+     */
+    int getMaxPlayers();
+
+    /**
+     * Gets a {@link Player} by their unique id
+     *
+     * @param uniqueId The UUID to get the player from
+     * @return {@link Player} if available
+     */
+    Optional<Player> getPlayer(UUID uniqueId);
+
+    /**
+     * Gets a {@link Player} by their name
+     *
+     * This only works for online players.
+     *
+     * <b>Note: Do not use names for persistent storage, the
+     * Zidane of today may not be the Zidane of yesterday.</b>
+     *
+     * @param name The name to get the player from
+     * @return {@link Player} if available
+     */
+    Optional<Player> getPlayer(String name);
+
+    /**
+     * Gets all currently loaded {@link World}s.
+     *
+     * @return Collection of loaded worlds
+     */
+    Collection<World> getWorlds();
+
+    /**
+     * Gets a loaded {@link World} by its unique id ({@link UUID}), if any.
+     *
+     * @param uniqueId UUID to lookup
+     * @return The world or null if not found
+     */
+    Optional<World> getWorld(UUID uniqueId);
+
+    /**
+     * Gets a loaded {@link World} by name, if any
+     *
+     * @param worldName Name to lookup
+     * @return The world or null if not found
+     */
+    Optional<World> getWorld(String worldName);
+
+    /**
+     * Sends the given message to all online players
+     *
+     * @param message The message to send
+     */
+    void broadcastMessage(Message<?> message);
+
+    /**
+     * Gets the port the server is listening on.
+     * @return The port
+     */
+    int getPort();
+
+    /**
+     * Gets the bound {@link InetSocketAddress} this server is accepting connections from.
+     * @return The address
+     */
+    InetSocketAddress getBoundIP();
+
+    /**
+     * Tests if the server has a whitelist enabled.
+     * @return True if enabled, false if not
+     */
+    boolean hasWhitelist();
+
+    /**
+     * Sets whether the server is utilizing a whitelist.
+     * @param enabled True to enable the whitelist, false to disable
+     */
+    void setHasWhitelist(boolean enabled);
+
+    /**
+     * Tests if this server is set to online mode.
+     *
+     * <b>Online mode authenticates users against Minecraft's servers, false performs
+     * no validity checks.</b>
+     * @return True if enabled, false if not
+     */
+    boolean getOnlineMode();
+
+}
