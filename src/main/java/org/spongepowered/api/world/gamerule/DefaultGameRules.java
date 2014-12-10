93ecf59bf39b2331990018e9a5425938e194d1c6
Me4502 <mnmiller1@hotmail.com>
2014-12-10 13:44:23 +1000

    Fill default list.

--- src/main/java/org/spongepowered/api/world/gamerule/DefaultGameRules.java
+++ src/main/java/org/spongepowered/api/world/gamerule/DefaultGameRules.java
@@ -32,21 +32,21 @@ import java.util.List;
  */
 public class DefaultGameRules {
 
-    public static final String COMMAND_BLOCK_OUTPUT = null;
-    public static final String DO_DAYLIGHT_CYCLE = null;
-    public static final String DO_ENTITY_DROPS = null;
-    public static final String DO_FIRE_TICK = null;
-    public static final String DO_MOB_LOOT = null;
-    public static final String DO_MOB_SPAWNING = null;
-    public static final String DO_TILE_DROPS = null;
-    public static final String KEEP_INVENTORY = null;
-    public static final String LOG_ADMIN_COMMANDS = null;
-    public static final String MOB_GRIEFING = null;
-    public static final String NATURAL_REGENERATION = null;
-    public static final String RANDOM_TICK_SPEED = null;
-    public static final String REDUCED_DEBUG_INFO = null;
-    public static final String SEND_COMMAND_FEEDBACK = null;
-    public static final String SHOW_DEATH_MESSAGES = null;
+    public static final String COMMAND_BLOCK_OUTPUT = "commandBlockOutput";
+    public static final String DO_DAYLIGHT_CYCLE = "doDaylightCycle";
+    public static final String DO_ENTITY_DROPS = "doEntityDrops";
+    public static final String DO_FIRE_TICK = "doFireTick";
+    public static final String DO_MOB_LOOT = "doMobLoot";
+    public static final String DO_MOB_SPAWNING = "doMobSpawning";
+    public static final String DO_TILE_DROPS = "doTileDrops";
+    public static final String KEEP_INVENTORY = "keepInventory";
+    public static final String LOG_ADMIN_COMMANDS = "logAdminCommands";
+    public static final String MOB_GRIEFING = "mobGriefing";
+    public static final String NATURAL_REGENERATION = "naturalRegeneration";
+    public static final String RANDOM_TICK_SPEED = "randomTickSpeed";
+    public static final String REDUCED_DEBUG_INFO = "reducedDebugInfo";
+    public static final String SEND_COMMAND_FEEDBACK = "sendCommandFeedback";
+    public static final String SHOW_DEATH_MESSAGES = "showDeathMessages";
 
     public static List<String> getValues() {
         return null;
