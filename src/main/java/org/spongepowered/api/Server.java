5bbbe8a492abffa1825513560551dc2f13efc707
ST-DDT <ST-DDT@gmx.de>
2014-12-14 12:52:49 +0100

    If events have to return a chunk decorator/populator then you also need them to create a new world.

--- src/main/java/org/spongepowered/api/Server.java
+++ src/main/java/org/spongepowered/api/Server.java
@@ -37,8 +37,6 @@ import java.net.InetSocketAddress;
 import java.util.Collection;
 import java.util.UUID;
 
-import javax.annotation.Nullable;
-
 /**
  * Represents a typical Minecraft Server.
  */
@@ -107,15 +105,13 @@ public interface Server {
      * 
      * @param name The name of the world to generate
      * @param generator The generator to generate new chunks.
-     * @param populatorm The populator to use. Can be null if no populator
-     *            should be used.
-     * @param decorator The decorator to use. Can be null if no decorator should
-     *            be used.
+     * @param populatorm The populator to use
+     * @param decorator The decorator to use
      * @return The created or loaded world
      * @throws IllegalStateException If the world is already loaded or something
      *             went wrong during the creation process.
      */
-    World createOrLoadWorld(String name, ChunkGenerator generator, @Nullable ChunkPopulator populatorm, @Nullable ChunkDecorator decorator) throws IllegalStateException;
+    World createOrLoadWorld(String name, ChunkGenerator generator, ChunkPopulator populator, ChunkDecorator decorator) throws IllegalStateException;
 
     /**
      * Gets the time, in ticks, since this server began running for the current session.
