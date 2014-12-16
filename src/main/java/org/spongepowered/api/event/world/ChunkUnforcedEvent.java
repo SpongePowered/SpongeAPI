769e773522c4efc42ff3324706edb524254f6b16
Kobata <kobata2@gmail.com>
2014-12-16 01:39:08 -0500

    Add API for force-loading chunks

---
+++ src/main/java/org/spongepowered/api/event/world/ChunkUnforcedEvent.java
@@ -0,0 +1,48 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered.org <http://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package org.spongepowered.api.event.world;
+
+import com.flowpowered.math.vector.Vector2i;
+import org.spongepowered.api.event.GameEvent;
+import org.spongepowered.api.world.ChunkManager;
+
+// NOTE: This does not extend ChunkEvent as Chunk may not be currently loaded.
+public interface ChunkUnforcedEvent extends GameEvent {
+
+    /**
+     * Gets the ticket that the chunk was removed from.
+     *
+     * @return The ticket the chunk was removed from
+     */
+    ChunkManager.LoadingTicket getTicket();
+
+    /**
+     * Gets the removed chunk coordinates.
+     *
+     * @return The coordinated of the removed chunk
+     */
+    Vector2i getChunkCoords();
+}
