16ebc9f7303d3e768b5f284ab3db1f5064891b5f
Kenzie Togami <ket1999@gmail.com>
2014-12-22 18:46:06 -0800

    Add Selector API. Merges #297, Closes #294

---
+++ src/main/java/org/spongepowered/api/text/selector/Selectors.java
@@ -0,0 +1,54 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered.org <http://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.api.text.selector;
+
+/**
+ * Utility class to work with and create Selectors.
+ */
+public final class Selectors {
+    private static final SelectorFactory factory = null;
+
+    private Selectors() {}
+
+    /**
+     * Creates a {@link SelectorBuilder} with no data.
+     *
+     * @return A new selector builder with no data
+     */
+    public static SelectorBuilder builder() {
+        return factory.createEmptyBuilder();
+    }
+
+    /**
+     * Parses a {@link Selector} from the given selector string.
+     *
+     * @param selector The raw selector string
+     * @return A new selector containing the given selector data
+     */
+    public static Selector parseRaw(String selector) {
+        return factory.parseRawSelector(selector);
+    }
+
+}
