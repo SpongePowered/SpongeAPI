708e628e1c133efcc1f8f0a464af87f181350063
Soren025 <soren.endon@gmail.com>
2014-12-24 17:49:19 -0600

    Javadoced Extents

--- src/main/java/org/spongepowered/api/world/extent/Extents.java
+++ src/main/java/org/spongepowered/api/world/extent/Extents.java
@@ -4,12 +4,33 @@ import java.util.Stack;
 
 import com.flowpowered.math.vector.Vector3d;
 
+/**
+ * Utility class for Extent.
+ */
 public class Extents {
     
+    /**
+     * Converts local vector coordinates into world coordinates relative to
+     * the given Extent.
+     * 
+     * @param extent The extent to use
+     * @param position The local coordinates
+     * @return The world coordinates relative to the given Extent
+     */
     public static Vector3d localToWorld(Extent extent, Vector3d position) {
         return localToWorld(extent, position.getX(), position.getY(), position.getZ());
     }
     
+    /**
+     * Converts local vector coordinates into world coordinates relative to
+     * the given Extent.
+     * 
+     * @param extent The extent to use
+     * @param x The local x coordinate
+     * @param y The local y coordinate
+     * @param z The local z coordinate
+     * @return The world coordinates relative to the given Extent
+     */
     public static Vector3d localToWorld(Extent extent, double x, double y, double z) {
         while (extent != null) {
             x -= extent.getOrigin().getX();
@@ -21,10 +42,28 @@ public class Extents {
         return new Vector3d(x, y, z);
     }
     
+    /**
+     * Converts world vector coordinates into local coordinates relative to
+     * the given Extent.
+     * 
+     * @param extent The extent to use
+     * @param position The world coordinates
+     * @return The local coordinates relative to the given Extent
+     */
     public static Vector3d worldToLocal(Extent extent, Vector3d position) {
         return worldToLocal(extent, position.getX(), position.getY(), position.getZ());
     }
     
+    /**
+     * Converts world vector coordinates into local coordinates relative to
+     * the given Extent.
+     * 
+     * @param extent The extent to use
+     * @param x The world x coordinate
+     * @param y The world y coordinate
+     * @param z The world z coordinate
+     * @return The local coordinates relative to the given Extent
+     */
     public static Vector3d worldToLocal(Extent extent, double x, double y, double z) {
         Stack<Extent> stack = new Stack<Extent>();
         while (extent != null) {
