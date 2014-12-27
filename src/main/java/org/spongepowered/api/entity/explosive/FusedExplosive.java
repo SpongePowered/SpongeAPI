8000cfeba1b7a422f64c79bb39eb8246f0bd4419
Gabriel Harris-Rouquette <gabizou@me.com>
2014-12-27 13:26:44 -0800

    Remove FusedExplosive#ignite as a FusedExplosive is already ignited when spawned.

--- src/main/java/org/spongepowered/api/entity/explosive/FusedExplosive.java
+++ src/main/java/org/spongepowered/api/entity/explosive/FusedExplosive.java
@@ -26,13 +26,10 @@ package org.spongepowered.api.entity.explosive;
 
 /**
  * Represents an explosive that detonates after its fuse has expired.
+ * <p>A FusedExplosive already implies that it has been ignited and will
+ * explode after the fuse has expired.</p>
  */
 public interface FusedExplosive extends Explosive {
-
-    /**
-     * Ignites this explosive to detonate after some fuse duration in ticks.
-     */
-    void ignite();
 
     /**
      * Ignites this explosive to detonate after the given fuse ticks.
