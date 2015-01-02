f4b7f2cad349a07b60c3a42868b8de254173ef0e
Bammerbom <j.brouwer@filternet.nl>
2015-01-02 20:28:09 +0100

    Remove EntityMoveEvent unneeded import.

--- src/main/java/org/spongepowered/api/event/entity/EntityMoveEvent.java
+++ src/main/java/org/spongepowered/api/event/entity/EntityMoveEvent.java
@@ -25,7 +25,6 @@
 
 package org.spongepowered.api.event.entity;
 
-import org.spongepowered.api.entity.Entity;
 import org.spongepowered.api.util.event.Cancellable;
 import org.spongepowered.api.world.Location;
 
