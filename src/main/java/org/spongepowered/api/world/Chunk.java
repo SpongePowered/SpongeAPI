7cc8cf388012fec1d720d9118550c65e73edb78a
Jamie <lexwaresupport@outlook.com>
2015-01-25 11:33:48 +0000

    Get the World from Chunk. Closes #413.

--- src/main/java/org/spongepowered/api/world/Chunk.java
+++ src/main/java/org/spongepowered/api/world/Chunk.java
@@ -56,6 +56,13 @@ public interface Chunk extends Extent {
     boolean isLoaded();
     
     /**
+     * Gets the world the chunk is in.
+     * 
+     * @return The world
+     */ 
+    World getWorld();
+    
+    /**
      * Gets if the chunk has been populated by the generator.
      * 
      * @return Whether or not the chunk has been populated.
