9330543c9611e6baa930e8b23c96308d130cec02
Minecrell <dev@minecrell.net>
2015-01-27 13:20:34 +0100

    Revert "Add initial Webservice functionality (draft)"
    This reverts commit f90435e473964b897f39fb68404a1fcce8fa27db.
    
    Moved draft to feature/webservice

--- src/main/java/org/spongepowered/api/service/webservice/WebServiceListener.java
+++
@@ -1,9 +0,0 @@
-package org.spongepowered.api.service.webservice;
-
-/**
- * Webservice Listener interface
- * Created by forsterth on 27/01/2015.
- */
-public interface WebServiceListener {
-    
-}
