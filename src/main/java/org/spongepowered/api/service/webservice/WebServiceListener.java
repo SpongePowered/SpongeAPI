f90435e473964b897f39fb68404a1fcce8fa27db
Theresa Forster <theresajayne@minuet-uk.com>
2015-01-27 10:54:14 +0000

    Add initial Webservice functionality (draft)

---
+++ src/main/java/org/spongepowered/api/service/webservice/WebServiceListener.java
@@ -0,0 +1,9 @@
+package org.spongepowered.api.service.webservice;
+
+/**
+ * Webservice Listener interface
+ * Created by forsterth on 27/01/2015.
+ */
+public interface WebServiceListener {
+    
+}
