64b5503ba62e134832bcee8425ca14f936b7fc2f
Gabriel Harris-Rouquette <gabizou@me.com>
2015-01-27 23:39:44 -0800

    Rename ChunkIterator to ChunkDataStream. Add some methods to fetch specific chunk data at a specified coordinate. This allows for ChunkDataStream to be used in an asynchronous thread as its methods actively block the thread while reading data.

--- src/main/java/org/spongepowered/api/world/storage/WorldStorage.java
+++ src/main/java/org/spongepowered/api/world/storage/WorldStorage.java
@@ -24,12 +24,58 @@
  */
 package org.spongepowered.api.world.storage;
 
+import com.flowpowered.math.vector.Vector3i;
+import com.google.common.base.Optional;
+import com.google.common.util.concurrent.ListenableFuture;
+import org.spongepowered.api.service.persistence.data.DataContainer;
+import org.spongepowered.api.world.Chunk;
+
 public interface WorldStorage {
 
     /**
-     * Gets a {@link ChunkIterator}.
+     * Gets a {@link ChunkDataStream}.
+     *
+     * <p>Usage of a {@link ChunkDataStream} should be limited to asynchronous
+     * tasks to avoid hanging the main thread.</p>
+     *
      * @return An iterator of generated chunks
      */
-    ChunkIterator getGeneratedChunks();
+    ChunkDataStream getGeneratedChunks();
+
+    /**
+     * Checks if the given chunk coordinates represented by {@link Vector3i}
+     * exist in the world.
+     *
+     * <p>Note that this is an asynchronous check as the storage of chunks
+     * can not be guaranteed to remain in synch with the server, let alone
+     * on the server thread.</p>
+     *
+     * <p>It is imperative to avoid waiting for the {@link ListenableFuture} to complete
+     * its task.</p>
+     *
+     * @param chunkCoords The chunk coordinates
+     * @return Whether the chunk exists or not
+     */
+    ListenableFuture<Boolean> doesChunkExist(Vector3i chunkCoords);
+
+    /**
+     * Gets a {@link DataContainer} including all data related to a {@link Chunk}.
+     *
+     * <p>The container is a read only instance of the data, and therefor
+     * should not be considered as mutable data. Changes are NOT saved, and
+     * the data may not be in synch with the server if the chunk is currently
+     * loaded.</p>
+     *
+     * <p>This may not return a {@link DataContainer} in the event there is no
+     * chunk data generated at the desired coordinates.</p>
+     *
+     * <p>It is imperative to understand that the {@link ListenableFuture} task is
+     * blocking, and should avoid using {@link ListenableFuture#get()} while on the main
+     * thread.</p>
+     *
+     * @param chunkCoords The chunk coordinates
+     * @return The data container representing the chunk data, if available
+     */
+    ListenableFuture<Optional<DataContainer>> getChunkData(Vector3i chunkCoords);
 
 }
