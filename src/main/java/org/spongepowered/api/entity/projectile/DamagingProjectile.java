ee30f64671d6b9f89e5d6608dcdc56891c5160c7
Gabriel Harris-Rouquette <gabizou@me.com>
2015-01-29 18:02:40 -0800

    Refactor Projectiles into DamagingProjectile and Projectile. Fixes #400

--- src/main/java/org/spongepowered/api/entity/projectile/DamagingProjectile.java
+++ src/main/java/org/spongepowered/api/entity/projectile/DamagingProjectile.java
@@ -22,13 +22,31 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
 package org.spongepowered.api.entity.projectile;
 
-import org.spongepowered.api.entity.explosive.FusedExplosive;
+import org.spongepowered.api.entity.Entity;
+import org.spongepowered.api.entity.projectile.source.ProjectileSource;
 
 /**
- * Represents a firework.
+ * Represents entities that act as projectiles and can fly in the air.
+ * For example, Arrows.
  */
-public interface Firework extends Projectile, FusedExplosive {
+public interface DamagingProjectile extends Projectile {
+
+    /**
+     * Gets the damage this projectile will deal to a {@link org.spongepowered.api.entity.living.Living}
+     * if hit.
+     *
+     * @return The damage of this arrow
+     */
+    double getDamage();
+
+    /**
+     * Sets the damage this projectile will deal to a LivingEntity if hit.
+     *
+     * @param damage The damage
+     */
+    void setDamage(double damage);
 
 }
