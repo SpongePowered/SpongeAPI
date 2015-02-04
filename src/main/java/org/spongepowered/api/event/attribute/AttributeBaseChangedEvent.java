53d75b6c3f5bdfd13ebed77195334f50a40f2fb3
AlphaModder <quasiflux@gmail.com>
2015-02-03 16:43:24 -0800

    Import Attribute in AttributeBaseChangedEvent to simplfy javadoc.

--- src/main/java/org/spongepowered/api/event/attribute/AttributeBaseChangedEvent.java
+++ src/main/java/org/spongepowered/api/event/attribute/AttributeBaseChangedEvent.java
@@ -25,19 +25,19 @@
 
 package org.spongepowered.api.event.attribute;
 
+import org.spongepowered.api.attribute.Attribute;
+
 public interface AttributeBaseChangedEvent extends AttributeEvent {
 
     /**
-     * Gets the old value of the changed
-     * {@link org.spongepowered.api.attribute.Attribute}.
+     * Gets the old value of the changed {@link Attribute}.
      * 
      * @return The old value of the changed Attribute
      */
     double getOldValue();
 
     /**
-     * Gets the new value of the changed
-     * {@link org.spongepowered.api.attribute.Attribute}.
+     * Gets the new value of the changed {@link Attribute}.
      * 
      * @return The new value of the changed Attribute
      */
