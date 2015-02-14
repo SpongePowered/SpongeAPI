940d0a45928a5568a787a13cd08f4a74b8302f9a
ST-DDT <ST-DDT@gmx.de>
2015-02-14 12:42:28 +0100

    More javadocs

--- src/main/java/org/spongepowered/api/attribute/AttributeSource.java
+++ src/main/java/org/spongepowered/api/attribute/AttributeSource.java
@@ -29,7 +29,16 @@ import java.util.Collection;
 
 /**
  * Represents a source of {@link AttributeModifier}s. For example some potions
- * like the speed potion also affect the entity's SPEED attribute
+ * like the speed potion also affect the entity's SPEED attribute.
+ *
+ * <p>
+ * <b>Note:</b> You can achieve the same effects (attribute wise) if you apply
+ * the {@link AttributeModifier}s from an AttributeSource (like a potion effect
+ * type) to an entity without adding the potion directly. If you apply an
+ * attribute modifier from an AttributeSource it will not be added another time,
+ * if you apply the entire AttributeSource to that entity, but it will be
+ * removed if you remove the AttributeSource from the entity.
+ * </p>
  */
 public interface AttributeSource {
 
