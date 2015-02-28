561441aaccc0e796c941a6e3e2298888169a569d
AlphaModder <quasiflux@gmail.com>
2015-02-27 19:36:28 -0800

    Refactor Skull.

--- src/main/java/org/spongepowered/api/item/data/Skull.java
+++ src/main/java/org/spongepowered/api/item/data/Skull.java
@@ -24,40 +24,16 @@
  */
 package org.spongepowered.api.item.data;
 
-import com.google.common.base.Optional;
+import org.spongepowered.api.GameProfile;
 
-import java.net.URL;
-import java.util.Date;
-import java.util.UUID;
+import com.google.common.base.Optional;
 
 import javax.annotation.Nullable;
 
 public interface Skull extends ItemBlockData {
 
-    Optional<UUID> getOwnerId();
+    Optional<GameProfile> getProfile();
 
-    void setOwnerId(@Nullable UUID id);
-
-    boolean isSigned();
-
-    Optional<Date> getTimestamp();
-
-    void setTimestamp(@Nullable Date timestamp);
-
-    String getOwnerName();
-
-    void setOwnerName(String name);
-
-    boolean isPublic();
-
-    void setPublic(boolean isPublic);
-
-    URL getSkinTexture();
-
-    void setSkinTexture(URL location);
-
-    Optional<URL> getCapeTexture();
-
-    void setCapeTexture(URL location);
+    void setProfile(@Nullable GameProfile profile);
 
 }
