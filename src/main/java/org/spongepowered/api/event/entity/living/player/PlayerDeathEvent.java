bfa075726dce21a2d5dd503466620b53b8886bde
Aaron Hill <aa1ronham@gmail.com>
2015-02-28 21:10:18 -0500

    Prefix keepsInventory and keepsLevel with 'get'

--- src/main/java/org/spongepowered/api/event/entity/living/player/PlayerDeathEvent.java
+++ src/main/java/org/spongepowered/api/event/entity/living/player/PlayerDeathEvent.java
@@ -53,7 +53,7 @@ public interface PlayerDeathEvent extends HumanDeathEvent, PlayerEvent {
      *
      * @return Whether the player keeps their inventory on death
      */
-    boolean keepsInventory();
+    boolean getKeepsInventory();
 
     /**
      * Sets if the player keeps their inventory on death.
@@ -67,7 +67,7 @@ public interface PlayerDeathEvent extends HumanDeathEvent, PlayerEvent {
      *
      * @return Whether the player keeps all of their EXP on death
      */
-    boolean keepsLevel();
+    boolean getKeepsLevel();
 
     /**
      * Sets if the player keeps all of their EXP on death.
