202b7e1a1b615db44e670e6dc8ef52062c9736a8
AlphaModder <quasiflux@gmail.com>
2015-03-06 14:00:29 -0800

    Fix newline.

--- src/main/java/org/spongepowered/api/block/ScheduledBlockUpdate.java
+++ src/main/java/org/spongepowered/api/block/ScheduledBlockUpdate.java
@@ -22,6 +22,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
 package org.spongepowered.api.block;
 
 import com.flowpowered.math.vector.Vector3i;
