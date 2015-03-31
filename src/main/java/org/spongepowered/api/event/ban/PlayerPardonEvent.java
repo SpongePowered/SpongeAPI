3173f8b225e5f9cfaad2a04eb88feeb65c1f0033
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 16:44:26 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/event/ban/PlayerPardonEvent.java
+++ src/main/java/org/spongepowered/api/event/ban/PlayerPardonEvent.java
@@ -32,4 +32,10 @@ import org.spongepowered.api.util.event.Cancellable;
  */
 public interface PlayerPardonEvent extends UserPardonEvent, PlayerEvent, Cancellable {
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
