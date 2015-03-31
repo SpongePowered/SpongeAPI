09a0d5161040aebff410dcd38e6962670ccc34ab
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 15:51:09 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/attribute/AttributeSource.java
+++ src/main/java/org/spongepowered/api/attribute/AttributeSource.java
@@ -49,4 +49,10 @@ public interface AttributeSource {
      */
     Collection<AttributeModifier> getAttributeModifiers();
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
