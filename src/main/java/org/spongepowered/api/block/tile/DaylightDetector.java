0a65dddc903214606ccdee68728dc887197b8f65
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 15:57:12 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/block/tile/DaylightDetector.java
+++ src/main/java/org/spongepowered/api/block/tile/DaylightDetector.java
@@ -30,4 +30,10 @@ package org.spongepowered.api.block.tile;
  */
 public interface DaylightDetector extends TileEntity {
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
