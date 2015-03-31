684fd50ee935bdaa7739c579270e619502560089
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 16:55:40 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/event/entity/living/human/HumanInteractEvent.java
+++ src/main/java/org/spongepowered/api/event/entity/living/human/HumanInteractEvent.java
@@ -33,4 +33,10 @@ import org.spongepowered.api.event.entity.living.LivingInteractEvent;
  */
 public interface HumanInteractEvent extends HumanEvent, LivingInteractEvent {
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
