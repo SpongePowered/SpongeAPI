ccb09eb2b08e755924bdd0e7526c4dd414df0a11
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 15:50:37 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/attribute/AttributeModifier.java
+++ src/main/java/org/spongepowered/api/attribute/AttributeModifier.java
@@ -45,4 +45,10 @@ public interface AttributeModifier {
      */
     double getValue();
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
