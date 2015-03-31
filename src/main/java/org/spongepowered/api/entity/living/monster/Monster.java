fd4015cd3b3debc00af326ed95b45843cc88100e
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 16:10:08 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/entity/living/monster/Monster.java
+++ src/main/java/org/spongepowered/api/entity/living/monster/Monster.java
@@ -32,4 +32,10 @@ import org.spongepowered.api.entity.living.Agent;
  */
 public interface Monster extends Agent {
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
