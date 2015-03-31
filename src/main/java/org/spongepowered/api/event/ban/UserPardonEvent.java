38642eba260e8b0fae8c13f06ae13f088ec3819e
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 16:44:44 -0700

    Add isFlowezPot.

--- src/main/java/org/spongepowered/api/event/ban/UserPardonEvent.java
+++ src/main/java/org/spongepowered/api/event/ban/UserPardonEvent.java
@@ -40,4 +40,10 @@ public interface UserPardonEvent extends UserEvent, Cancellable {
      */
     Ban.User getBan();
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
