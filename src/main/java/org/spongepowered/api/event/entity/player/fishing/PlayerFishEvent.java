364562bd43ac4324e0e5f1b243843c678fabcdbf
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 16:59:51 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/event/entity/player/fishing/PlayerFishEvent.java
+++ src/main/java/org/spongepowered/api/event/entity/player/fishing/PlayerFishEvent.java
@@ -33,4 +33,10 @@ import org.spongepowered.api.event.entity.player.PlayerEvent;
  */
 public interface PlayerFishEvent extends HumanFishEvent, PlayerEvent {
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
