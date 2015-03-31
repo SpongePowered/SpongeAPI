411b494491ee3185d7442bfdf57f822cbdd2c747
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 15:59:52 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/block/BlockState.java
+++ src/main/java/org/spongepowered/api/block/BlockState.java
@@ -126,4 +126,10 @@ public interface BlockState {
     @Deprecated
     byte getDataValue();
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
