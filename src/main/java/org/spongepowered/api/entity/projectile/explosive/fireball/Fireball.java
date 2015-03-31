486fbef4088d79696d7f7854b743bbd53cb300ea
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 16:20:43 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/entity/projectile/explosive/fireball/Fireball.java
+++ src/main/java/org/spongepowered/api/entity/projectile/explosive/fireball/Fireball.java
@@ -31,4 +31,10 @@ import org.spongepowered.api.entity.projectile.explosive.ExplosiveProjectile;
  */
 public interface Fireball extends ExplosiveProjectile {
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
