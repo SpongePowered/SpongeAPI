bdd2c389447da517e2097d6445b35311f8a18883
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 16:59:30 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/event/entity/living/LivingUpdateEvent.java
+++ src/main/java/org/spongepowered/api/event/entity/living/LivingUpdateEvent.java
@@ -33,4 +33,10 @@ import org.spongepowered.api.event.entity.EntityUpdateEvent;
  */
 public interface LivingUpdateEvent extends LivingEvent, EntityUpdateEvent {
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
