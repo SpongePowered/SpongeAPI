6e2375e7ad28fb612cc6d35054e69ac7ec3d7378
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 16:08:30 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/entity/living/golem/SnowGolem.java
+++ src/main/java/org/spongepowered/api/entity/living/golem/SnowGolem.java
@@ -30,4 +30,10 @@ package org.spongepowered.api.entity.living.golem;
  */
 public interface SnowGolem extends Golem {
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
