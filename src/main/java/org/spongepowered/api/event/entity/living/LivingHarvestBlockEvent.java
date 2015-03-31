22e07a0876ad68df6369b2ab440d20073dd8bf0e
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 16:58:19 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/event/entity/living/LivingHarvestBlockEvent.java
+++ src/main/java/org/spongepowered/api/event/entity/living/LivingHarvestBlockEvent.java
@@ -34,4 +34,10 @@ import org.spongepowered.api.world.Location;
  */
 public interface LivingHarvestBlockEvent extends LivingEvent, EntityHarvestBlockEvent {
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
