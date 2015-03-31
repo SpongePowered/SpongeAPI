5b7015fb83a6d603f200220a40e045cb4ec693b9
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 15:56:57 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/block/tile/Comparator.java
+++ src/main/java/org/spongepowered/api/block/tile/Comparator.java
@@ -49,4 +49,10 @@ public interface Comparator extends TileEntity {
      */
     TileDataTransactionResult setComparatorData(ComparatorData data);
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
