a08ac4ba1ab8657072675ac048f6c3a13f840637
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 16:54:50 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/event/entity/living/human/HumanDropItemEvent.java
+++ src/main/java/org/spongepowered/api/event/entity/living/human/HumanDropItemEvent.java
@@ -33,4 +33,10 @@ import org.spongepowered.api.event.entity.living.LivingDropItemEvent;
  */
 public interface HumanDropItemEvent extends HumanEvent, LivingDropItemEvent {
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
