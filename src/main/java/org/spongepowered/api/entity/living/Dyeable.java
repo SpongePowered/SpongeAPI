f13a7d9740027f8e852cab60cc409351d8c35018
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 16:19:04 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/entity/living/Dyeable.java
+++ src/main/java/org/spongepowered/api/entity/living/Dyeable.java
@@ -46,4 +46,10 @@ public interface Dyeable {
      */
     void setColor(DyeColor color);
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
