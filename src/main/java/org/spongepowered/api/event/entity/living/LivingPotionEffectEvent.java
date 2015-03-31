150c5d16712d35626fd92c641d50f800fe57f541
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 16:59:19 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/event/entity/living/LivingPotionEffectEvent.java
+++ src/main/java/org/spongepowered/api/event/entity/living/LivingPotionEffectEvent.java
@@ -47,4 +47,10 @@ public interface LivingPotionEffectEvent extends LivingEvent {
      */
     PotionEffect getPotionEffect();
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
