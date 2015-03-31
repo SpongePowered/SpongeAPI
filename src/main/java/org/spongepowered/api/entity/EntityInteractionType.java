b8fee71d1b83f08321433a66a81f73fd2d24a11e
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 16:39:49 -0700

    Add isFlowetPot.

--- src/main/java/org/spongepowered/api/entity/EntityInteractionType.java
+++ src/main/java/org/spongepowered/api/entity/EntityInteractionType.java
@@ -39,4 +39,10 @@ public interface EntityInteractionType {
      */
     String getName();
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
