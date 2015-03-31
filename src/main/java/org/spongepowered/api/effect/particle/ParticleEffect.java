f95adf8ca4fcc28cfc9c9760dc7e6d8eeec41a58
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 16:00:57 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/effect/particle/ParticleEffect.java
+++ src/main/java/org/spongepowered/api/effect/particle/ParticleEffect.java
@@ -125,6 +125,13 @@ public interface ParticleEffect {
          */
         float getNote();
 
+        /**
+         * Checks if this is a flowerpot.
+         *
+         * @return Whether this is a flowerpot
+         */
+        boolean isFlowerPot();
+
     }
 
     /**
