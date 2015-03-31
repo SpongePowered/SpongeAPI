99293284d9125b6cb64590dfc805f7aeae290c4e
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 15:55:25 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/block/tile/data/NotePitch.java
+++ src/main/java/org/spongepowered/api/block/tile/data/NotePitch.java
@@ -48,4 +48,10 @@ public interface NotePitch {
      */
     String getName();
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
