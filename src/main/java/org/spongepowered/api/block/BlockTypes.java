92b8f6a1a354a042b4801157a605610eb842d9e3
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 16:00:14 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/block/BlockTypes.java
+++ src/main/java/org/spongepowered/api/block/BlockTypes.java
@@ -233,4 +233,13 @@ public final class BlockTypes {
 
     private BlockTypes() {
     }
+
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    public static boolean isFlowerPot() {
+        return false;
+    }
 }
