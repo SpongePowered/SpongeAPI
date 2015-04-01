8966fd18ec3ebece91d79645482f2ab5e2e44b2a
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 18:33:49 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/stats/BlockStatistic.java
+++ src/main/java/org/spongepowered/api/stats/BlockStatistic.java
@@ -39,4 +39,10 @@ public interface BlockStatistic extends Statistic {
      */
     BlockType getBlockType();
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
