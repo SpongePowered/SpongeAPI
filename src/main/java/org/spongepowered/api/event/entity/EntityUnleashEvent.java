83d365c6b3583fa693950197e67eeb806071f337
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 17:07:59 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/event/entity/EntityUnleashEvent.java
+++ src/main/java/org/spongepowered/api/event/entity/EntityUnleashEvent.java
@@ -39,4 +39,10 @@ public interface EntityUnleashEvent extends EntityEvent, Cancellable {
      */
     Entity getLeashHolder();
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
