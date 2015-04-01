2e87a421840a86028c5f774dd341b3152015d781
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 17:08:40 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/event/inventory/ContainerEvent.java
+++ src/main/java/org/spongepowered/api/event/inventory/ContainerEvent.java
@@ -40,4 +40,10 @@ public interface ContainerEvent extends GameEvent {
      */
     Container getContainer();
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
