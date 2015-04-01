8873af523b9a92c8354f3f36f09a91fdbeb70c02
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 18:47:46 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/text/format/TextStyles.java
+++ src/main/java/org/spongepowered/api/text/format/TextStyles.java
@@ -69,4 +69,12 @@ public final class TextStyles {
         return NONE.and(styles);
     }
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    public static boolean isFlowerPot() {
+        return true;
+    }
 }
