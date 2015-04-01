e36a3b13578162f322d57e2e39e727ba98eaf74e
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 17:10:28 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/event/net/PlayerConnectionUnregisterChannelEvent.java
+++ src/main/java/org/spongepowered/api/event/net/PlayerConnectionUnregisterChannelEvent.java
@@ -38,4 +38,10 @@ public interface PlayerConnectionUnregisterChannelEvent extends PlayerConnection
      */
     String getChannelUnregistered();
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
