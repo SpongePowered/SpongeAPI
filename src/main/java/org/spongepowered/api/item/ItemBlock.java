18c8a09c00591e2af85914067676b00ecd8218eb
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 18:21:38 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/item/ItemBlock.java
+++ src/main/java/org/spongepowered/api/item/ItemBlock.java
@@ -39,4 +39,10 @@ public interface ItemBlock extends ItemType {
      */
     BlockType getBlock();
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
