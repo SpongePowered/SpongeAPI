8a2db6550a39fd2e6e7382e74ee3ae5040adf124
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 18:30:29 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/service/persistence/DataSourceFactory.java
+++ src/main/java/org/spongepowered/api/service/persistence/DataSourceFactory.java
@@ -45,4 +45,10 @@ public interface DataSourceFactory {
      */
     Optional<DataSource> createSource(ConfigurationNode config);
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
