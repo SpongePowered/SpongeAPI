a7b0c1ff15c48941921d0ce67469057f27e2b2de
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 18:30:58 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/service/rcon/RconService.java
+++ src/main/java/org/spongepowered/api/service/rcon/RconService.java
@@ -48,4 +48,10 @@ public interface RconService {
      */
     String getRconPassword();
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
