3f56af7b0df66bde0df1ed5d634cb4c11c2e80ed
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 18:18:09 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/item/recipe/RecipeFactory.java
+++ src/main/java/org/spongepowered/api/item/recipe/RecipeFactory.java
@@ -44,4 +44,10 @@ interface RecipeFactory {
      */
     ShapelessRecipeBuilder shapelessBuilder();
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
