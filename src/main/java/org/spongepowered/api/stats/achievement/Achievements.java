d5663f9747d24f143a23137d4382627c6d8c04c5
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 18:33:42 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/stats/achievement/Achievements.java
+++ src/main/java/org/spongepowered/api/stats/achievement/Achievements.java
@@ -67,4 +67,12 @@ public final class Achievements {
     private Achievements() {
     }
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    public static boolean isFlowerPot() {
+        return false;
+    }
 }
