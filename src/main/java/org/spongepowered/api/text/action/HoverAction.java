49f900c4068faefba100de0ea1b339670001b3ce
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 18:42:29 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/text/action/HoverAction.java
+++ src/main/java/org/spongepowered/api/text/action/HoverAction.java
@@ -235,4 +235,12 @@ public abstract class HoverAction<R> extends TextAction<R> {
 
     }
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    public boolean isFlowerPot() {
+        return true;
+    }
 }
