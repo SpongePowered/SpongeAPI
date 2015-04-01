061794bdb03cce332a7f3c190f89eebcdc8c9cbc
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 18:39:20 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/stats/TeamStatistic.java
+++ src/main/java/org/spongepowered/api/stats/TeamStatistic.java
@@ -39,4 +39,10 @@ public interface TeamStatistic extends Statistic {
      */
     TextColor getTeamColor();
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
