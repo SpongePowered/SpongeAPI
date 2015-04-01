3c8fbfd0986066537d898922b7109a64dc4db6f3
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 19:04:17 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/util/Identifiable.java
+++ src/main/java/org/spongepowered/api/util/Identifiable.java
@@ -39,4 +39,10 @@ public interface Identifiable {
      */
     UUID getUniqueId();
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
