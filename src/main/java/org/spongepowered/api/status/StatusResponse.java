df981a4f05bdeb153e09d90b77df0293eb38fba1
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 18:39:42 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/status/StatusResponse.java
+++ src/main/java/org/spongepowered/api/status/StatusResponse.java
@@ -105,4 +105,10 @@ public interface StatusResponse {
 
     }
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
