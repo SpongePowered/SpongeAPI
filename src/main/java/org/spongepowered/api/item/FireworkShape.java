e1567f6f05dbdf937da35d0589f060674f0614e7
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 18:20:31 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/item/FireworkShape.java
+++ src/main/java/org/spongepowered/api/item/FireworkShape.java
@@ -39,4 +39,10 @@ public interface FireworkShape {
      */
     String getId();
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
