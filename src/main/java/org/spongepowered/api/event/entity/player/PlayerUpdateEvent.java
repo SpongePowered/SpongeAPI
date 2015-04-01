696c218704cabe5113409702a69fe309a00da229
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 17:03:07 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/event/entity/player/PlayerUpdateEvent.java
+++ src/main/java/org/spongepowered/api/event/entity/player/PlayerUpdateEvent.java
@@ -33,4 +33,10 @@ import org.spongepowered.api.event.entity.living.human.HumanUpdateEvent;
  */
 public interface PlayerUpdateEvent extends PlayerEvent, HumanUpdateEvent {
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
