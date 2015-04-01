85a535ae0b7dc32a9c55c946134025f51ca8cdad
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 17:09:45 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/event/inventory/ItemResultEvent.java
+++ src/main/java/org/spongepowered/api/event/inventory/ItemResultEvent.java
@@ -48,4 +48,10 @@ public interface ItemResultEvent extends InventoryEvent {
      */
     void setResult(ItemStack result);
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
