21e0a3b95ef3ad56e4265e99d8cc60c17cb9ef79
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 18:22:33 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/net/ChannelListener.java
+++ src/main/java/org/spongepowered/api/net/ChannelListener.java
@@ -44,4 +44,10 @@ public interface ChannelListener {
      */
     void handlePayload(PlayerConnection client, String channel, ChannelBuf data);
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
