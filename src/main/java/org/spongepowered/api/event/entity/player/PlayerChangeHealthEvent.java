3e3fa7be2bfaafe1c206f1a7cd992df578dcee25
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 17:00:24 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/event/entity/player/PlayerChangeHealthEvent.java
+++ src/main/java/org/spongepowered/api/event/entity/player/PlayerChangeHealthEvent.java
@@ -33,4 +33,10 @@ import org.spongepowered.api.event.entity.living.human.HumanChangeHealthEvent;
  */
 public interface PlayerChangeHealthEvent extends PlayerEvent, HumanChangeHealthEvent {
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
