3934dbe7526f1e569f0276b926d0afce58095968
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 18:06:41 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/item/data/SpawnableData.java
+++ src/main/java/org/spongepowered/api/item/data/SpawnableData.java
@@ -32,4 +32,10 @@ import org.spongepowered.api.entity.EntityType;
  */
 public interface SpawnableData extends PseudoEnumItemData<EntityType, SpawnableData> {
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
