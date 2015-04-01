41ab29ba62925853d0cac71e47e3cc9df077a685
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 19:37:20 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/scoreboard/critieria/Criterion.java
+++ src/main/java/org/spongepowered/api/scoreboard/critieria/Criterion.java
@@ -39,4 +39,10 @@ public interface Criterion {
      */
     String getName();
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
--- src/main/java/org/spongepowered/api/scoreboard/displayslot/DisplaySlotBuilder.java
+++ src/main/java/org/spongepowered/api/scoreboard/displayslot/DisplaySlotBuilder.java
@@ -44,4 +44,10 @@ public interface DisplaySlotBuilder {
      */
     DisplaySlot build() throws IllegalStateException;
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
