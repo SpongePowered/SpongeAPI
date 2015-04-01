46c440c419e21e0d774d8016c92b9b141fc9ced5
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 17:05:42 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/event/entity/EntityExitPortalEvent.java
+++ src/main/java/org/spongepowered/api/event/entity/EntityExitPortalEvent.java
@@ -32,4 +32,10 @@ package org.spongepowered.api.event.entity;
  */
 public interface EntityExitPortalEvent extends EntityEvent {
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
