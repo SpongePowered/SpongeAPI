ebbaf778c92b25f3f717b371efc6e398c778a190
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 18:26:26 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/service/config/ConfigService.java
+++ src/main/java/org/spongepowered/api/service/config/ConfigService.java
@@ -76,4 +76,10 @@ public interface ConfigService {
      */
     ConfigRoot getPluginConfig(Object instance);
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
