fb8711422baa661d4501df7301be7993492182e5
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 17:09:40 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/event/inventory/ItemMergeEvent.java
+++ src/main/java/org/spongepowered/api/event/inventory/ItemMergeEvent.java
@@ -39,4 +39,10 @@ public interface ItemMergeEvent extends ItemEvent, Cancellable {
      */
     Item getItemToMerge();
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
