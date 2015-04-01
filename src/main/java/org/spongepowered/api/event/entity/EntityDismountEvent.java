e4aa8784131710b25a0a871b6f2d9c57484552fe
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 17:04:59 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/event/entity/EntityDismountEvent.java
+++ src/main/java/org/spongepowered/api/event/entity/EntityDismountEvent.java
@@ -39,4 +39,10 @@ public interface EntityDismountEvent extends EntityEvent, Cancellable {
      */
     Entity getDismounted();
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
