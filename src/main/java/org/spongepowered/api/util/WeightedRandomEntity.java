27bfaf2244a6a0d8443d6f9f6da321e10394469c
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 19:05:07 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/util/WeightedRandomEntity.java
+++ src/main/java/org/spongepowered/api/util/WeightedRandomEntity.java
@@ -55,4 +55,10 @@ public interface WeightedRandomEntity {
      */
     int getWeight();
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
