375b2a0999fc43f1016cc53f7a43e79014cbacc1
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 17:00:35 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/event/entity/player/PlayerChatEvent.java
+++ src/main/java/org/spongepowered/api/event/entity/player/PlayerChatEvent.java
@@ -33,4 +33,10 @@ import org.spongepowered.api.event.message.MessageEvent;
  */
 public interface PlayerChatEvent extends MessageEvent, PlayerEvent {
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
