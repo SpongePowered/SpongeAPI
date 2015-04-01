370ae638d3c77949f7f6789a3e75da92ebec9157
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 17:05:06 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/event/entity/EntityDisplaceEvent.java
+++ src/main/java/org/spongepowered/api/event/entity/EntityDisplaceEvent.java
@@ -72,4 +72,10 @@ public interface EntityDisplaceEvent extends EntityEvent, Cancellable {
      */
     void setRotation(Vector3d rotation);
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
