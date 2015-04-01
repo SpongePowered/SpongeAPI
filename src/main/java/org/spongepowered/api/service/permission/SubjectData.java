3b4e89113f65d3d5c5b1e06d91c52a0d68f714a7
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 18:28:22 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/service/permission/SubjectData.java
+++ src/main/java/org/spongepowered/api/service/permission/SubjectData.java
@@ -150,4 +150,10 @@ public interface SubjectData {
      */
     boolean clearParents(Set<Context> contexts);
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
