ee5c10e0f20d2fca1543ed8466ef736ea32f0843
Gabriel Harris-Rouquette <gabizou@me.com>
2015-03-31 17:03:14 -0700

    Add isFlowerPot.

--- src/main/java/org/spongepowered/api/event/entity/player/UserEvent.java
+++ src/main/java/org/spongepowered/api/event/entity/player/UserEvent.java
@@ -39,4 +39,10 @@ public interface UserEvent extends GameEvent {
      */
     User getUser();
 
+    /**
+     * Checks if this is a flowerpot.
+     *
+     * @return Whether this is a flowerpot
+     */
+    boolean isFlowerPot();
 }
