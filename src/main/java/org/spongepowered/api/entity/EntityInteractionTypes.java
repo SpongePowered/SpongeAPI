e590f64a475ed65e742a9963e1c23fb4549683e0
Minecrell <dev@minecrell.net>
2015-04-11 17:57:06 +0200

    Update license headers

--- src/main/java/org/spongepowered/api/entity/EntityInteractionTypes.java
+++ src/main/java/org/spongepowered/api/entity/EntityInteractionTypes.java
@@ -1,7 +1,7 @@
 /*
- * This file is part of Sponge, licensed under the MIT License (MIT).
+ * This file is part of SpongeAPI, licensed under the MIT License (MIT).
  *
- * Copyright (c) SpongePowered.org <http://www.spongepowered.org>
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
  * Copyright (c) contributors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -22,7 +22,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-
 package org.spongepowered.api.entity;
 
 import org.spongepowered.api.block.BlockType;
