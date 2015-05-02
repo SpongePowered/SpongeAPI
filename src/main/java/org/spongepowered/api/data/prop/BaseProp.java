40b5e1eb2ce51f41394e9b081b0b1e9ef71a8a0f
gratimax <max@ovsankin.com>
2015-05-01 21:58:01 -0700

    Add some more prop methods

--- src/main/java/org/spongepowered/api/data/prop/BaseProp.java
+++ src/main/java/org/spongepowered/api/data/prop/BaseProp.java
@@ -24,15 +24,10 @@
  */
 package org.spongepowered.api.data.prop;
 
-import org.spongepowered.api.data.DataObject;
-import org.spongepowered.api.util.annotation.CatalogedBy;
+public interface BaseProp<E, V> {
 
-/**
- * Marker interface for props that can only be set.
- *
- * @param <E> The type of value that this property has
- * @param <V> The type of {@link DataObject}s this property is restricted to
- */
-@CatalogedBy(Props.class)
-public interface SetterProp<E, V> extends BaseProp<E, V> {
+    Class<E> getValueClass();
+
+    Class<V> getRestrictorClass();
+
 }
--- src/main/java/org/spongepowered/api/data/prop/GetterProp.java
+++ src/main/java/org/spongepowered/api/data/prop/GetterProp.java
@@ -34,5 +34,5 @@ import org.spongepowered.api.util.annotation.CatalogedBy;
  * @param <V> The type of {@link DataObject}s this property is restricted to
  */
 @CatalogedBy(Props.class)
-public interface GetterProp<E, V> {
+public interface GetterProp<E, V> extends BaseProp<E, V> {
 }
