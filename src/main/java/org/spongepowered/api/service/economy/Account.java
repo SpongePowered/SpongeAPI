076a3a5890e32d38579665663e9b2b7e08292467
Xemiru Sanzenin <xemiruk@gmail.com>
2015-05-09 06:29:50 -0700

    more formatting.

--- src/main/java/org/spongepowered/api/service/economy/Account.java
+++ src/main/java/org/spongepowered/api/service/economy/Account.java
@@ -29,6 +29,7 @@ import org.spongepowered.api.entity.player.Player;
 import java.util.UUID;
 
 public interface Account {
+
     /**
      * The string that prefixes all custom account names.
      */
@@ -40,33 +41,27 @@ public interface Account {
     public static final String SERVER_ACCOUNT_NAME = "_ESCSV";
 
     /**
-     * Retrieves the string representative of the owner of
-     * this {@link Account}.
+     * Retrieves the string representative of the owner of this {@link Account}.
      * 
-     * <p>The String returned can represent 3 types of
-     * accounts; namely a Player's, the Server's, or a
-     * customly-registered account.</p>
+     * <p>The String returned can represent 3 types of accounts; namely a
+     * Player's, the Server's, or a customly-registered account.</p>
      * 
-     * <p>If the String returned matches the value denoted
-     * by {@link Account#SERVER_ACCOUNT_NAME}, then the
-     * owner of this Account is the server itself.</p>
+     * <p>If the String returned matches the value denoted by
+     * {@link Account#SERVER_ACCOUNT_NAME}, then the owner of this Account is
+     * the server itself.</p>
      * 
-     * <p>If the String returned begins with the value
-     * denoted by {@link Account#CUSTOM_DATA_PREFIX},
-     * then the owner of this Account is a custom owner,
-     * as set by a plugin.</p>
+     * <p>If the String returned begins with the value denoted by
+     * {@link Account#CUSTOM_DATA_PREFIX}, then the owner of this Account is a
+     * custom owner, as set by a plugin.</p>
      * 
-     * <p>If the String returned does not match either of
-     * the above, then it is expected to be the {@link UUID}
-     * of a {@link Player}, in the form given by
+     * <p>If the String returned does not match either of the above, then it is
+     * expected to be the {@link UUID} of a {@link Player}, in the form given by
      * {@link UUID#toString()}.</p>
      * 
-     * <p>Convenience methods should exist within
-     * implementation of the service; it is not necessary to
-     * check the String on your own.</p>
+     * <p>Convenience methods should exist within implementation of the service;
+     * it is not necessary to check the String on your own.</p>
      * 
-     * @return a String representative of the owner of this
-     *         Account
+     * @return a String representative of the owner of this Account
      * 
      * @see UUID
      * @see Player
@@ -77,76 +72,66 @@ public interface Account {
     String getOwner();
 
     /**
-     * Returns whether or not the owner of this
-     * {@link Account} is a {@link Player}.
+     * Returns whether or not the owner of this {@link Account} is a
+     * {@link Player}.
      * 
-     * <p>If this returns true, then
-     * {@link Account#getOwner()} can safely be expected
-     * to return the {@link UUID} of a Player, in the form
-     * given by {@link UUID#toString()}.</p>
+     * <p>If this returns true, then {@link Account#getOwner()} can safely be
+     * expected to return the {@link UUID} of a Player, in the form given by
+     * {@link UUID#toString()}.</p>
      * 
-     * @return true if Account.getOwner() returns a
-     *         Player UUID
+     * @return true if Account.getOwner() returns a Player UUID
      * 
      * @see Account#getOwner()
      */
     boolean isPlayer();
 
     /**
-     * Returns whether or not the owner of this
-     * {@link Account} is the server itself.
+     * Returns whether or not the owner of this {@link Account} is the server
+     * itself.
      * 
-     * <p>If this returns true, then
-     * {@link Account#getOwner()} can safely be expected
-     * to return the value denoted by
+     * <p>If this returns true, then {@link Account#getOwner()} can safely be
+     * expected to return the value denoted by
      * {@link Account#SERVER_ACCOUNT_NAME}.</p>
      * 
-     * @return true if Account.getOwner() returns the
-     *         value of Account.SERVER_ACCOUNT_NAME
+     * @return true if Account.getOwner() returns the value of
+     *         Account.SERVER_ACCOUNT_NAME
      * 
      * @see Account#getOwner()
      */
     boolean isServer();
 
     /**
-     * Returns whether or not the owner of this
-     * {@link Account} is a custom account, as set by
-     * the implementing service.
+     * Returns whether or not the owner of this {@link Account} is a custom
+     * account, as set by the implementing service.
      * 
-     * <p>If this returns true, then
-     * {@link Account#getOwner()} can safely be expected
-     * to return a custom account, of which begins with the
-     * value denoted by {@link Account#CUSTOM_DATA_PREFIX}
-     * .</p>
+     * <p>If this returns true, then {@link Account#getOwner()} can safely be
+     * expected to return a custom account, of which begins with the value
+     * denoted by {@link Account#CUSTOM_DATA_PREFIX} .</p>
      * 
-     * @return true if Account.getOwner() returns a
-     *         value beginning with the String stored in
-     *         Account.CUSTOM_DATA_PREFIX
+     * @return true if Account.getOwner() returns a value beginning with the
+     *         String stored in Account.CUSTOM_DATA_PREFIX
      * 
      * @see Account#getOwner()
      */
     boolean isCustom();
 
     /**
-     * Returns a double representative of the balance stored
-     * within this {@link Account}.
+     * Returns a double representative of the balance stored within this
+     * {@link Account}.
      * 
-     * <p>The double returned is not to exceed two decimal
-     * places. An example would be <pre>129.54</pre>.</p>
+     * <p>The double returned is not to exceed two decimal places. An example
+     * would be <pre>129.54</pre>.</p>
      * 
-     * @return a double value, with two or less decimal
-     *         places
+     * @return a double value, with two or less decimal places
      */
     double getBalance();
 
     /**
-     * Sets the balance stored by this {@link Account} as
-     * the given value.
+     * Sets the balance stored by this {@link Account} as the given value.
      * 
-     * <p>The amount set cannot exceed two decimal places
-     * (e.g. <pre>129.54</pre>); as such, the double passed
-     * as the amount will be rounded up or down if it
-     * violates the former.</p>
+     * <p>The amount set cannot exceed two decimal places (e.g.
+     * <pre>129.54</pre>); as such, the double passed as the amount will be
+     * rounded up or down if it violates the former.</p>
      * 
      * <p>The amount is allowed to be negative.</p>
      * 
@@ -155,51 +140,41 @@ public interface Account {
     void setBalance(double amount);
 
     /**
-     * Pays the target {@link Account} the given amount
-     * of currency by taking the amount from this
-     * Account.
+     * Pays the target {@link Account} the given amount of currency by taking
+     * the amount from this Account.
      * 
-     * <p>If this Account results in holding a negative
-     * balance due to this operation, this will return
-     * true. This does not mean the operation is
+     * <p>If this Account results in holding a negative balance due to this
+     * operation, this will return true. This does not mean the operation is
      * cancelled.</p>
      * 
      * <p>A negative amount parameter will result in an
      * IllegalArgumentException.</p>
      * 
-     * @param target the Account to send the amount of
-     *        money to
-     * @param amount a double representative of the amount
-     *        to give to the target Account
+     * @param target the Account to send the amount of money to
+     * @param amount a double representative of the amount to give to the target
+     *        Account
      * 
-     * @return true if this Account's balance turned
-     *         negative
+     * @return true if this Account's balance turned negative
      * 
-     * @throws IllegalArgumentException if the amount to pay
-     *         is negative
+     * @throws IllegalArgumentException if the amount to pay is negative
      */
     boolean pay(Account target, double amount);
 
     /**
-     * Removes the given sum of currency from this
-     * {@link Account}.
+     * Removes the given sum of currency from this {@link Account}.
      * 
-     * <p>If this Account results in holding a negative
-     * balance due to this operation, this will return
-     * true. This does not mean the operation is
+     * <p>If this Account results in holding a negative balance due to this
+     * operation, this will return true. This does not mean the operation is
      * cancelled.</p>
      * 
      * <p>A negative amount parameter will result in an
      * IllegalArgumentException.</p>
      * 
-     * @param amount a double representing the amount to
-     *        remove from the account
+     * @param amount a double representing the amount to remove from the account
      * 
-     * @return true if this Account's balance turned
-     *         negative
+     * @return true if this Account's balance turned negative
      * 
-     * @throws IllegalArgumentException if the amount to
-     *         remove is negative
+     * @throws IllegalArgumentException if the amount to remove is negative
      */
     boolean elicit(double amount);
 }
--- src/main/java/org/spongepowered/api/service/economy/EconomyService.java
+++ src/main/java/org/spongepowered/api/service/economy/EconomyService.java
@@ -31,88 +31,79 @@ import java.util.List;
 import java.util.UUID;
 
 public interface EconomyService {
+
     /**
-     * Retrieves the {@link Account} denoted by the
-     * given owner string.
+     * Retrieves the {@link Account} denoted by the given owner string.
      * 
-     * <p>If the Account does not exist, this method will
-     * return Optional.absent().</p>
+     * <p>If the Account does not exist, this method will return
+     * Optional.absent().</p>
      * 
-     * <p>Owner strings are not representative of players
-     * only. See {@link Account#getOwner()} for
-     * reference.</p>
+     * <p>Owner strings are not representative of players only. See
+     * {@link Account#getOwner()} for reference.</p>
      * 
      * @param owner the owner string to search with
      * 
-     * @return the Account owned by the given owner, or
-     *         Optional.absent() otherwise
+     * @return the Account owned by the given owner, or Optional.absent()
+     *         otherwise
      * 
      * @see Account#getOwner()
      * @see EconomyService#createNewAccount(String)
      */
     Optional<Account> getAccount(String owner);
-    
+
     /**
-     * Creates a new {@link Account} and assigns it to the
-     * given owner string.
+     * Creates a new {@link Account} and assigns it to the given owner string.
      * 
-     * <p>This method cannot override existing Accounts. If
-     * the Account already exists, this method will return
-     * Optional.absent() to indicate that creation has
-     * failed. Existing Accounts should be reset before
-     * they are replaced.</p>
+     * <p>This method cannot override existing Accounts. If the Account already
+     * exists, this method will return Optional.absent() to indicate that
+     * creation has failed. Existing Accounts should be reset before they are
+     * replaced.</p>
      * 
-     * <p>Owner strings are not representative of players
-     * only. See {@link Account#getOwner()} for
-     * reference.</p>
+     * <p>Owner strings are not representative of players only. See
+     * {@link Account#getOwner()} for reference.</p>
      * 
-     * @param owner the owner string to assign the new
-     *        Account to
+     * @param owner the owner string to assign the new Account to
      * 
-     * @return whether or not the Account was created
-     *         successfully
+     * @return whether or not the Account was created successfully
      * 
      * @see Account
      * @see Account#getOwner()
      * @see EconomyService#resetAccount(String)
      */
     Optional<Account> createNewAccount(String owner);
-    
+
     /**
-     * Retrieves the {@link Account} denoted by
-     * the given {@link UUID}, belonging to a {@link Player}
-     * , in the form given by {@link UUID#toString()}.
-     * Shortcut method for {@link #getAccount(String)}.
+     * Retrieves the {@link Account} denoted by the given {@link UUID},
+     * belonging to a {@link Player} , in the form given by
+     * {@link UUID#toString()}. Shortcut method for {@link #getAccount(String)}.
      * 
      * @param uuid the UUID of the Player owning the Account
      * 
-     * @return the Player Account found with the given name,
-     *         or Optional.absent() otherwise
+     * @return the Player Account found with the given name, or
+     *         Optional.absent() otherwise
      * 
      * @see Account
      * @see EconomyService#getAccount(String);
      */
     Optional<Account> getPlayerAccount(UUID uuid);
-    
+
     /**
      * Retrieves the {@link Account} denoted by
-     * {@link Account#CUSTOM_DATA_PREFIX} + the given
-     * account name string. Shortcut method for
-     * {@link #getAccount(String)}.
+     * {@link Account#CUSTOM_DATA_PREFIX} + the given account name string.
+     * Shortcut method for {@link #getAccount(String)}.
      * 
      * @param accountName the name of the custom account
      * 
-     * @return the custom Account found with the given name,
-     *         or Optional.absent() otherwise
+     * @return the custom Account found with the given name, or
+     *         Optional.absent() otherwise
      * 
      * @see Account
      * @see EconomyService#getAccount(String);
      */
     Optional<Account> getCustomAccount(String accountName);
-    
+
     /**
-     * Retrieves the {@link Account} assigned to the
-     * server. Shortcut method for
+     * Retrieves the {@link Account} assigned to the server. Shortcut method for
      * {@link #getAccount(String)}.
      * 
      * @return the Account belonging to the server
@@ -121,42 +112,38 @@ public interface EconomyService {
      * @see EconomyService#getAccount(String);
      */
     Account getServerAccount();
-    
+
     /**
-     * Returns an immutable list of all {@link Account}s
-     * belonging to a {@link Player}.
+     * Returns an immutable list of all {@link Account}s belonging to a
+     * {@link Player}.
      * 
-     * <p>This method should try to return all Accounts
-     * registered belonging to Players; including offline
-     * Players.</p>
+     * <p>This method should try to return all Accounts registered belonging to
+     * Players; including offline Players.</p>
      * 
      * @return an immutable List of Accounts
      * 
      * @see Account
      */
     List<Account> getAllPlayerAccounts();
-    
+
     /**
-     * Returns an immutable list of all custom
-     * {@link Account}s made by plugins.
+     * Returns an immutable list of all custom {@link Account}s made by plugins.
      * 
      * @return an immutable List of Accounts
      * 
      * @see Account
      */
     List<Account> getAllCustomAccounts();
-    
+
     /**
-     * Resets the data of the {@link Account} denoted by the
-     * given owner string, essentially deleting it.
+     * Resets the data of the {@link Account} denoted by the given owner string,
+     * essentially deleting it.
      * 
-     * <p>If the Account was reset successfully, this method
-     * will return true. It will return false if the Account
-     * did not exist.</p>
+     * <p>If the Account was reset successfully, this method will return true.
+     * It will return false if the Account did not exist.</p>
      * 
-     * <p>Owner strings are not representative of players
-     * only. See {@link Account#getOwner()} for
-     * reference.</p>
+     * <p>Owner strings are not representative of players only. See
+     * {@link Account#getOwner()} for reference.</p>
      * 
      * @param owner the owner string to search with
      * 
