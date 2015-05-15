b98a786c5f241505f419487359098326b4cddc2a
ST-DDT <ST-DDT@gmx.de>
2015-05-15 13:31:55 +0200

    Add Events

---
+++ src/main/java/org/spongepowered/api/event/entity/living/LivingLevelChangeEvent.java
@@ -0,0 +1,57 @@
+/*
+ * This file is part of SpongeAPI, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package org.spongepowered.api.event.entity.living;
+
+import org.spongepowered.api.entity.living.Living;
+import org.spongepowered.api.event.Cancellable;
+
+/**
+ * Called when a {@link Living} changes its level.
+ */
+public interface LivingLevelChangeEvent extends LivingEvent, Cancellable {
+
+    /**
+     * Gets the level the {@link Living} had before.
+     *
+     * @return The level the living had before
+     */
+    int getOldLevel();
+
+    /**
+     * Gets the new level of the {@link Living}.
+     *
+     * @return The new level of the living
+     */
+    int getNewLevel();
+
+    /**
+     * Sets the new level of the {@link Living}.
+     *
+     * @param level The new level of the living
+     */
+    void setNewLevel(int level);
+
+}
---
+++ src/main/java/org/spongepowered/api/event/merchant/MerchantTradeOfferChangeEvent.java
@@ -0,0 +1,68 @@
+/*
+ * This file is part of SpongeAPI, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package org.spongepowered.api.event.merchant;
+
+import org.spongepowered.api.event.Cancellable;
+import org.spongepowered.api.event.cause.CauseTracked;
+import org.spongepowered.api.item.merchant.Merchant;
+import org.spongepowered.api.item.merchant.TradeOffer;
+
+import java.util.List;
+
+/**
+ * Called when a {@link Merchant} changes his {@link TradeOffer}s.
+ */
+public interface MerchantTradeOfferChangeEvent extends CauseTracked, Cancellable {
+
+    /**
+     * Gets the {@link Merchant} whose trade offer has changed.
+     *
+     * @return The merchant whose trade offer has changed
+     */
+    Merchant getMerchant();
+
+    /**
+     * Gets an immutable list of {@link TradeOffer}s the merchant had before.
+     *
+     * @return A list of trade offers the merchant had before
+     */
+    List<TradeOffer> getOldTradeOffers();
+
+    /**
+     * Gets an immutable list of {@link TradeOffer}s the merchant had before.
+     *
+     * @return A list of trade offers the merchant had before
+     */
+    List<TradeOffer> getNewTradeOffers();
+
+    /**
+     * Sets a list of trade offers the merchant should have.
+     *
+     * @param offers A list of trade offers the merchant should have
+     */
+    void setNewTradeOffers(List<TradeOffer> offers);
+
+}
