0fcd46570938ebda328d16aee56ed90519422082
Mark Boudreau-Dalton <mboudreaudalton@gmail.com>
2015-09-05 00:09:07 -0400

    Create the SpongeExecutorService annotation bindings, to distinguish between sync. and async.

---
+++ src/main/java/org/spongepowered/api/service/scheduler/Asynchronous.java
@@ -0,0 +1,17 @@
+package org.spongepowered.api.service.scheduler;
+
+import com.google.inject.BindingAnnotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Provides a convenient injection for <b>asynchronous</b> {@link SpongeExecutorService}s.
+ */
+@BindingAnnotation
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+public @interface Asynchronous {
+}
