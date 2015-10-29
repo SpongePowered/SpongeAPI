748f2dc0624e3e21d9c2d947c3a87b7423054639
Brynley McDonald <brynleymcdonald@gmail.com>
2015-10-29 15:12:17 +1300

    Add fall distance data to API

---
+++ src/main/java/org/spongepowered/api/data/manipulator/immutable/entity/ImmutableFallDistanceData.java
@@ -0,0 +1,20 @@
+package org.spongepowered.api.data.manipulator.immutable.entity;
+
+import org.spongepowered.api.data.manipulator.ImmutableDataManipulator;
+import org.spongepowered.api.data.manipulator.mutable.entity.FallDistanceData;
+import org.spongepowered.api.data.value.immutable.ImmutableValue;
+
+/**
+ * A {@link ImmutableDataManipulator} that represents the distance an entity has fallen
+ * in the world. Usually the higher this value, the more damage that inflicted
+ * when the entity reaches the ground.
+ */
+public interface ImmutableFallDistanceData extends ImmutableDataManipulator<ImmutableFallDistanceData, FallDistanceData> {
+
+    /**
+     * Gets the {@link ImmutableValue} for the current fall distance.
+     * @return The value for the fall distance
+     */
+    ImmutableValue<Integer> fallDistance();
+
+}
---
+++ src/main/java/org/spongepowered/api/data/manipulator/mutable/entity/FallDistanceData.java
@@ -0,0 +1,20 @@
+package org.spongepowered.api.data.manipulator.mutable.entity;
+
+import org.spongepowered.api.data.manipulator.DataManipulator;
+import org.spongepowered.api.data.manipulator.immutable.entity.ImmutableFallDistanceData;
+import org.spongepowered.api.data.value.mutable.Value;
+
+/**
+ * A {@link DataManipulator} that represents the distance an entity has fallen
+ * in the world. Usually the higher this value, the more damage that inflicted
+ * when the entity reaches the ground.
+ */
+public interface FallDistanceData extends DataManipulator<FallDistanceData, ImmutableFallDistanceData> {
+
+    /**
+     * Gets the {@link Value} for the current fall distance.
+     * @return The value for the fall distance
+     */
+    Value<Integer> fallDistance();
+
+}
