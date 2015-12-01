8cbfafec442c456b460d1c71dcbfa35e9c7cb1d1
ST-DDT <ST-DDT@gmx.de>
2015-12-01 21:17:29 +0100

    More formal CatalogType definition, Merges #775

--- src/main/java/org/spongepowered/api/CatalogType.java
+++ src/main/java/org/spongepowered/api/CatalogType.java
@@ -25,24 +25,39 @@
 package org.spongepowered.api;
 
 /**
- * Represents a type of a catalog that can be used to identify types
- * without using an {@link Enum}.
+ * Represents a type of a catalog that can be used to identify types without
+ * using an {@link Enum}.
+ *
+ * <p>All implementing classes, including those not listed in the catalog
+ * specified by the {@link org.spongepowered.api.util.annotation.CatalogedBy
+ * CatalogedBy} annotation, must meet the requirement that if any of
+ * <code>`a.equals(b)`</code>, <code>`a == b`</code>, or
+ * <code>`a.getId().equalsIgnoreCase(b.getId())`</code> are true then all must
+ * be true.</p>
  */
 public interface CatalogType {
 
     /**
-     * Gets the unique identifier of this {@link CatalogType}. The identifier
-     * can be formatted however needed.
+     * Gets the unique identifier of this {@link CatalogType}. The identifier is
+     * case insensitive, thus there cannot be another instance with a different
+     * character case. The id of this instance must remain the same for the
+     * entire duration of its existence. The identifier can be formatted however
+     * needed.
+     *
+     * <p>A typical id format follows the pattern of <code>`modId:name`</code>
+     * or <code>`minecraft:name`</code>. However the prefix may be omitted for
+     * default/vanilla minecraft types.</p>
      *
      * @return The unique identifier of this catalog type
      */
     String getId();
 
     /**
-     * Gets the unique human-readable name of this individual
-     * {@link CatalogType}.
+     * Gets the human-readable name of this individual {@link CatalogType}. This
+     * name is not guaranteed to be unique. This value should not be used for
+     * serialization.
      *
-     * @return The uniquely identifiable name of this catalog type
+     * @return The human-readable name of this catalog type
      */
     String getName();
 
--- src/main/java/org/spongepowered/api/util/annotation/CatalogedBy.java
+++ src/main/java/org/spongepowered/api/util/annotation/CatalogedBy.java
@@ -24,6 +24,8 @@
  */
 package org.spongepowered.api.util.annotation;
 
+import org.spongepowered.api.CatalogType;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
@@ -33,10 +35,30 @@ import java.lang.annotation.Target;
 import javax.annotation.Nonnull;
 
 /**
- * Represents a class that is intended to represent a type of enum, without
- * using {@link Enum}. The class marked as {@link CatalogedBy} must have a
- * registrar class that can be queried for all types and subtypes of the
- * catalog.
+ * Annotates a {@link CatalogType} class to specify a set of special catalog
+ * classes which act as the catalog for the annotated type. The annotated class
+ * must extend {@link CatalogType}, and the specified catalog classes should be
+ * queryable or otherwise serve constant instances of the cataloged type.
+ *
+ * <p>All Classes mentioned in the CatalogedBy annotation must meet the
+ * following requirements:</p>
+ * <ul>
+ * <li>The values served by catalog classes must remain the same and valid
+ * at all times.</li>
+ * <li>The variables in catalog classes may link to null if the given value is
+ * no longer valid and no appropriate alternative can be found. If no
+ * alternative could be found and if there probably won't be a new one in the
+ * future then the field should be marked as deprecated and should be removed
+ * after a grace period or with the next big release of minecraft, SpongeAPI or
+ * the containing artifact.</li>
+ * <li>It is possible for two or more different variables to link to the same
+ * value. This includes both simple "well known" aliases and features that been
+ * merged together or that were originally very similar and one has been
+ * removed.</li>
+ * <li>It is also possible that one or more values are not listed in the
+ * catalog classes (Especially plugin or mod provided ones).</li>
+ * </ul>
+ * </p>
  */
 @Nonnull
 @Target(ElementType.TYPE)
@@ -46,9 +68,9 @@ public @interface CatalogedBy {
 
     /**
      * Gets the class that is the catalog for this {@link CatalogedBy} type.
-     * Since the type class annotated with {@link CatalogedBy} knows what
-     * the catalog class is, we can safely rely on the value to get all
-     * known instances of the {@link CatalogedBy}.
+     * Since the type class annotated with {@link CatalogedBy} knows what the
+     * catalog class is, we can safely rely on the value to get all known
+     * instances of the {@link CatalogedBy}.
      *
      * <p>This is similar to knowing at runtime that all available
      * "EntityType"(s) are cataloged in the "EntityTypes" class.</p>
