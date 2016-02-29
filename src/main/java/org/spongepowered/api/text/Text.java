7b4f4b60a12377981cd54fcff41bf4b3c554d1a5
Walker Crouse <walkercrouse@hotmail.com>
2016-02-29 13:48:57 -0500

    Fix trim() doc
    Signed-off-by: Walker Crouse <walkercrouse@hotmail.com>

--- src/main/java/org/spongepowered/api/text/Text.java
+++ src/main/java/org/spongepowered/api/text/Text.java
@@ -704,8 +704,8 @@ public abstract class Text implements TextRepresentable, DataSerializable, Compa
         }
 
         /**
-         * Formats existing variables within this builder and builds an
-         * immutable instance of the current state of this text builder.
+         * Removes all empty texts from the beginning and end of this
+         * builder.
          *
          * @return This builder
          */
