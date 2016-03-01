dfdaaefc791f545571fee42c306a2b7e446518bd
Brynley McDonald <brynleymcdonald@gmail.com>
2016-03-02 02:58:17 +1300

    Add API for handedness

---
+++ src/main/java/org/spongepowered/api/data/manipulator/mutable/entity/DominantHandData.java
@@ -0,0 +1,52 @@
+/*
+ * This file is part of SpongeAPI, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.api.data.manipulator.mutable.entity;
+
+import org.spongepowered.api.data.manipulator.DataManipulator;
+import org.spongepowered.api.data.manipulator.immutable.entity.ImmutableDominantHandData;
+import org.spongepowered.api.data.type.HandType;
+import org.spongepowered.api.data.value.mutable.Value;
+import org.spongepowered.api.entity.Entity;
+import org.spongepowered.api.entity.living.Living;
+
+/**
+ * A {@link DataManipulator} representing the dominant {@link HandType} of a
+ * {@link Living} entity.
+ * <p>
+ *     Handedness usually determines which hand is used for "main" interactions,
+ *     such as tool use or block breaking.
+ * </p>
+ */
+public interface DominantHandData extends DataManipulator<DominantHandData, ImmutableDominantHandData> {
+
+    /**
+     * Gets the {@link Value} representing the dominant {@link HandType} of an
+     * {@link Entity}.
+     *
+     * @return The value for handedness
+     */
+    Value<HandType> dominantHand();
+
+}
