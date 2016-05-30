fd2656a9500181a0f4c45400c25496e27e649f65
zml <zml@aoeu.xyz>
2016-05-29 21:50:27 -0700

    Allow passing a plugin parameter to SqlService
    This allows the implementation of SqlService to return DataSources that
    use paths or database names relative to the plugin's configuration
    directory. This is most useful for file-backed databases like H2, but
    may have other uses.

--- src/main/java/org/spongepowered/api/service/sql/SqlService.java
+++ src/main/java/org/spongepowered/api/service/sql/SqlService.java
@@ -52,6 +52,20 @@ public interface SqlService {
     DataSource getDataSource(String jdbcConnection) throws SQLException;
 
     /**
+     * Returns a data source for the provided JDBC connection string or an alias
+     *
+     * <p>A jdbc connection url is expected to be of the form:
+     * jdbc:&lt;engine&gt;://[&lt;username&gt;[:&lt;password&gt;]@]&lt;host&gt;/&lt;database&gt;
+     * or an alias (available aliases are known only by the service provider)</p>
+     *
+     * @param plugin The plugin to lookup databases relative to (primarily applying to file-backed databases)
+     * @param jdbcConnection The jdbc url or connection alias
+     * @return A data source providing connections to the given URL.
+     * @throws SQLException if a connection to the given database could not be established
+     */
+    DataSource getDataSource(Object plugin, String jdbcConnection) throws SQLException;
+
+    /**
      * Returns a possible connection URL for a given alias.
      *
      * @param alias The alias to check
