ff3a7b9366541caa63b534ea34adf98a7c66fed5
Chris Sanders <zidane@spongepowered.org>
2016-06-03 21:43:25 -0500

    Fix docs better from implementation.
    Signed-off-by: Chris Sanders <zidane@spongepowered.org>

--- src/main/java/org/spongepowered/api/Server.java
+++ src/main/java/org/spongepowered/api/Server.java
@@ -359,11 +359,8 @@ public interface Server {
      * Shuts down the server, and kicks all players with the default kick message.
      *
      * <p>
-     *     For the Sponge implementation on the client, there are some characteristics
-     *     of this method to keep in mind. The first being that invocation of this method
-     *     early in the Single-Player login process will send the user's client back to the
-     *     Multi-Player menu (by Vanilla design). The second quirk is this will be invoked the
-     *     tick after invocation.
+     *     For the Sponge implementation on the client, this will trigger the Integrated
+     *     Server to shutdown a tick later.
      * </p>
      *
      */
