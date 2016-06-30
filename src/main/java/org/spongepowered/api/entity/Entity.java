400bc7d39d6423415dfecd866db8d39c98ad3dfc
Aleksi Sapon <aleksi@sapon.ca>
2016-06-29 20:08:50 -0400

    Add Entity.getBoundBox()

--- src/main/java/org/spongepowered/api/entity/Entity.java
+++ src/main/java/org/spongepowered/api/entity/Entity.java
@@ -38,6 +38,7 @@ import org.spongepowered.api.event.cause.Cause;
 import org.spongepowered.api.event.cause.entity.damage.source.DamageSource;
 import org.spongepowered.api.event.entity.MoveEntityEvent;
 import org.spongepowered.api.text.translation.Translatable;
+import org.spongepowered.api.util.AABB;
 import org.spongepowered.api.util.Identifiable;
 import org.spongepowered.api.util.RelativePositions;
 import org.spongepowered.api.world.Locatable;
@@ -338,6 +339,15 @@ public interface Entity extends Identifiable, Locatable, DataHolder, DataSeriali
                 .map(world -> transferToWorld(world, position))
                 .orElse(false);
     }
+
+    /**
+     * Gets the entity's bounding box, usually for collisions and interaction.
+     * The box has an offset matching the entity's positions. That is to say,
+     * it is absolutely positioned and not relative to the entity.
+     *
+     * @return The axis aligned bounding box
+     */
+    Optional<AABB> getBoundingBox();
 
     /**
      * Gets the entity passenger that rides this entity, if available.
