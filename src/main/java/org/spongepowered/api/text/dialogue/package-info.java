cb6549922179003ea4c3f1e9efac4c94a77c04d2
Adam Spofford <pieflavor.mc@gmail.com>
2016-07-04 02:32:18 -0700

    Add FLARD

--- src/main/java/org/spongepowered/api/text/dialogue/package-info.java
+++ src/main/java/org/spongepowered/api/text/dialogue/package-info.java
@@ -22,30 +22,5 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+@org.spongepowered.api.util.annotation.NonnullByDefault
 package org.spongepowered.api.text.dialogue;
-
-import org.spongepowered.api.text.Text;
-import org.spongepowered.api.text.TextRepresentable;
-
-/**
- * A response to a {@link Question}. Mainly a container.
- */
-public interface Answer extends TextRepresentable {
-
-    @Override
-    default Text toText() {
-        return Text.of(getAnswer());
-    }
-
-    /**
-     * Gets the response that was sent.
-     * @return The response
-     */
-    String getAnswer();
-
-    /**
-     * Gets who sent the response.
-     * @return The speaker
-     */
-    Speaker getSpeaker();
-}
