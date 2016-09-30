b4c646c0c4a8ec5e7d1bc7cae34145b1f0c41b09
AlphaModder <quasiflux@gmail.com>
2016-09-30 11:52:41 -0700

    Format data manipulators.

--- src/main/java/org/spongepowered/api/data/manipulator/immutable/entity/ImmutableTagsData.java
+++ src/main/java/org/spongepowered/api/data/manipulator/immutable/entity/ImmutableTagsData.java
@@ -28,8 +28,8 @@ import org.spongepowered.api.data.manipulator.ImmutableDataManipulator;
 import org.spongepowered.api.data.manipulator.mutable.entity.TagsData;
 import org.spongepowered.api.data.value.immutable.ImmutableSetValue;
 
-public interface ImmutableTagsData extends ImmutableDataManipulator<ImmutableTagsData, TagsData>  {
+public interface ImmutableTagsData extends ImmutableDataManipulator<ImmutableTagsData, TagsData> {
 
-	ImmutableSetValue<String> tags();
-	
+    ImmutableSetValue<String> tags();
+
 }
--- src/main/java/org/spongepowered/api/data/manipulator/mutable/entity/TagsData.java
+++ src/main/java/org/spongepowered/api/data/manipulator/mutable/entity/TagsData.java
@@ -28,8 +28,8 @@ import org.spongepowered.api.data.manipulator.DataManipulator;
 import org.spongepowered.api.data.manipulator.immutable.entity.ImmutableTagsData;
 import org.spongepowered.api.data.value.mutable.SetValue;
 
-public interface TagsData extends DataManipulator<TagsData, ImmutableTagsData>{
+public interface TagsData extends DataManipulator<TagsData, ImmutableTagsData> {
 
-	SetValue<String> tags();
-	
+    SetValue<String> tags();
+
 }
