d4b70a33768112b865bcbf1496dac56a440669f7
kashike <kashike@vq.lc>
2016-11-13 08:34:21 +0000

    Add doWeatherCycle and maxEntityCramming gamerules Also specify the default values of gamerules

--- src/main/java/org/spongepowered/api/world/gamerule/DefaultGameRules.java
+++ src/main/java/org/spongepowered/api/world/gamerule/DefaultGameRules.java
@@ -26,6 +26,7 @@ package org.spongepowered.api.world.gamerule;
 
 import org.spongepowered.api.block.tileentity.CommandBlock;
 import org.spongepowered.api.entity.living.Agent;
+import org.spongepowered.api.entity.living.Bat;
 import org.spongepowered.api.entity.living.Villager;
 import org.spongepowered.api.entity.living.animal.Rabbit;
 import org.spongepowered.api.entity.living.animal.Sheep;
@@ -41,6 +42,8 @@ import org.spongepowered.api.entity.living.monster.Zombie;
 import org.spongepowered.api.entity.living.monster.ZombiePigman;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.entity.living.player.gamemode.GameModes;
+import org.spongepowered.api.entity.vehicle.Boat;
+import org.spongepowered.api.entity.vehicle.minecart.Minecart;
 
 /**
  * An enumeration of all possible GameRule names in vanilla minecraft.
@@ -51,7 +54,7 @@ public class DefaultGameRules {
      * Whether {@link CommandBlock}s should notify admins when
      * they perform commands.
      *
-     * <p>This is a boolean game rule.</p>
+     * <p>This is a boolean game rule, with a default value of {@code true}.</p>
      */
     public static final String COMMAND_BLOCK_OUTPUT = "commandBlockOutput";
 
@@ -59,66 +62,88 @@ public class DefaultGameRules {
      * Whether the server should skip checking player speed when
      * the player is wearing elytra.
      *
-     * <p>This is a boolean game rule.</p>
+     * <p>This is a boolean game rule, with a default value of {@code false}.</p>
      */
     public static final String DISABLE_ELYTRA_MOVEMENT_CHECK = "disableElytraMovementCheck";
 
     /**
      * Whether the day-night cycle and moon phases progress.
      *
-     * <p>This is a boolean game rule.</p>
+     * <p>This is a boolean game rule, with a default value of {@code true}.</p>
      */
     public static final String DO_DAYLIGHT_CYCLE = "doDaylightCycle";
 
     /**
      * Whether entities that are not mobs should have drops.
      *
-     * <p>This is a boolean game rule.</p>
+     * <p>This is a boolean game rule, with a default value of {@code true}.</p>
      */
     public static final String DO_ENTITY_DROPS = "doEntityDrops";
 
     /**
      * Whether fire should spread and naturally extinguish.
      *
-     * <p>This is a boolean game rule.</p>
+     * <p>This is a boolean game rule, with a default value of {@code true}.</p>
      */
     public static final String DO_FIRE_TICK = "doFireTick";
 
     /**
      * Whether {@link Agent}s should drop items.
      *
-     * <p>This is a boolean game rule.</p>
+     * <p>This is a boolean game rule, with a default value of {@code true}.</p>
      */
     public static final String DO_MOB_LOOT = "doMobLoot";
 
     /**
      * Whether {@link Agent}s should naturally spawn.
      *
-     * <p>This is a boolean game rule.</p>
+     * <p>This is a boolean game rule, with a default value of {@code true}.</p>
      */
     public static final String DO_MOB_SPAWNING = "doMobSpawning";
 
     /**
      * Whether blocks should have drops.
      *
-     * <p>This is a boolean game rule.</p>
+     * <p>This is a boolean game rule, with a default value of {@code true}.</p>
      */
     public static final String DO_TILE_DROPS = "doTileDrops";
+
+    /**
+     * Whether the weather will change.
+     *
+     * <p>This is a boolean game rule, with a default value of {@code true}.</p>
+     */
+    public static final String DO_WEATHER_CYCLE = "doWeatherCycle";
 
     /**
      * Whether {@link Player}s should keep items in their inventory
      * after death.
      *
-     * <p>This is a boolean game rule.</p>
+     * <p>This is a boolean game rule, with a default value of {@code false}.</p>
      */
     public static final String KEEP_INVENTORY = "keepInventory";
 
     /**
      * Whether to log admin commands to server log.
      *
-     * <p>This is a boolean game rule.</p>
+     * <p>This is a boolean game rule, with a default value of {@code true}.</p>
      */
     public static final String LOG_ADMIN_COMMANDS = "logAdminCommands";
+
+    /**
+     * The maximum number of other pushable entities a mob or player can push,
+     * before taking 3 suffocation damage per half-second.
+     *
+     * <p>Damage affects {@link GameModes#SURVIVAL survival mode} or
+     * {@link GameModes#ADVENTURE adventure mode} {@link Player}s, and all
+     * mobs but bats. Pushable entities include non-spectator-mode {@link Player}, any
+     * mob except {@link Bat}s, as well as {@link Boat}s and {@link Minecart}.</p>
+     *
+     * <p>Setting to {@code 0} disables the rule.</p>
+     *
+     * <p>This is a numerical game rule, with a default value of {@code 24}.</p>
+     */
+    public static final String MAX_ENTITY_CRAMMING = "maxEntityCramming";
 
     /**
      * Whether {@link Agent}s should be able to change blocks, and whether
@@ -149,7 +174,7 @@ public class DefaultGameRules {
      *     <li>{@link ZombiePigman}</li>
      * </ul></p>
      *
-     * <p>This is a boolean game rule.</p>
+     * <p>This is a boolean game rule, with a default value of {@code true}.</p>
      */
     public static final String MOB_GRIEFING = "mobGriefing";
 
@@ -158,7 +183,7 @@ public class DefaultGameRules {
      * hunger is full enough (doesn't affect external healing, such as
      * golden apples, the Regeneration effect, etc.).
      *
-     * <p>This is a boolean game rule.</p>
+     * <p>This is a boolean game rule, with a default value of {@code true}.</p>
      */
     public static final String NATURAL_REGENERATION = "naturalRegeneration";
 
@@ -168,14 +193,14 @@ public class DefaultGameRules {
      *
      * <p>0 will disable random ticks, higher numbers will increase random ticks</p>
      *
-     * <p>This is a numerical game rule.</p>
+     * <p>This is a numerical game rule, with a default value of {@code 3}.</p>
      */
     public static final String RANDOM_TICK_SPEED = "randomTickSpeed";
 
     /**
      * Whether the debug screen shows all or reduced information.
      *
-     * <p>This is a boolean game rule.</p>
+     * <p>This is a boolean game rule, with a default value of {@code false}.</p>
      */
     public static final String REDUCED_DEBUG_INFO = "reducedDebugInfo";
 
@@ -186,14 +211,14 @@ public class DefaultGameRules {
      * <p>This game rule affects the default behavior of whether
      * {@link CommandBlock}s store their output text.</p>
      *
-     * <p>This is a boolean game rule.</p>
+     * <p>This is a boolean game rule, with a default value of {@code true}.</p>
      */
     public static final String SEND_COMMAND_FEEDBACK = "sendCommandFeedback";
 
     /**
      * Whether a message appears in chat when a {@link Player} dies.
      *
-     * <p>This is a boolean game rule.</p>
+     * <p>This is a boolean game rule, with a default value of {@code true}.</p>
      */
     public static final String SHOW_DEATH_MESSAGES = "showDeathMessages";
 
@@ -202,7 +227,7 @@ public class DefaultGameRules {
      * that a player will spawn in when first joining a server or when
      * dying without a spawnpoint.
      *
-     * <p>This is a numerical game rule.</p>
+     * <p>This is a numerical game rule, with a default value of {@code 10}.</p>
      */
     public static final String SPAWN_RADIUS = "spawnRadius";
 
@@ -210,7 +235,7 @@ public class DefaultGameRules {
      * Whether players in {@link GameModes#SPECTATOR spectator mode} can
      * generate chunks.
      *
-     * <p>This is a boolean game rule.</p>
+     * <p>This is a boolean game rule, with a default value of {@code true}.</p>
      */
     public static final String SPECTATORS_GENERATE_CHUNKS = "spectatorsGenerateChunks";
 
