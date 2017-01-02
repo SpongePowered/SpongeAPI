9614e90c87423bba2225363495e6d646510fa1b1
Jakub Hlusička <limeth@protonmail.com>
2017-01-02 20:10:02 +0100

    add a getter for the smelting recipe registry to the GameRegistry

--- src/main/java/org/spongepowered/api/GameRegistry.java
+++ src/main/java/org/spongepowered/api/GameRegistry.java
@@ -35,6 +35,8 @@ import org.spongepowered.api.item.ItemType;
 import org.spongepowered.api.item.merchant.TradeOfferGenerator;
 import org.spongepowered.api.item.merchant.VillagerRegistry;
 import org.spongepowered.api.item.recipe.RecipeRegistry;
+import org.spongepowered.api.item.recipe.crafting.CraftingRecipe;
+import org.spongepowered.api.item.recipe.smelting.SmeltingRecipe;
 import org.spongepowered.api.network.status.Favicon;
 import org.spongepowered.api.plugin.PluginContainer;
 import org.spongepowered.api.plugin.PluginManager;
@@ -44,10 +46,10 @@ import org.spongepowered.api.registry.RegistryModule;
 import org.spongepowered.api.registry.RegistryModuleAlreadyRegisteredException;
 import org.spongepowered.api.resourcepack.ResourcePack;
 import org.spongepowered.api.scoreboard.displayslot.DisplaySlot;
-import org.spongepowered.api.statistic.Statistic;
 import org.spongepowered.api.statistic.BlockStatistic;
 import org.spongepowered.api.statistic.EntityStatistic;
 import org.spongepowered.api.statistic.ItemStatistic;
+import org.spongepowered.api.statistic.Statistic;
 import org.spongepowered.api.statistic.StatisticType;
 import org.spongepowered.api.text.format.TextColor;
 import org.spongepowered.api.text.selector.SelectorFactory;
@@ -292,11 +294,18 @@ public interface GameRegistry {
     Favicon loadFavicon(BufferedImage image) throws IOException;
 
     /**
-     * Retrieves the RecipeRegistry for this GameRegistry.
+     * Retrieves the crafting RecipeRegistry for this GameRegistry.
      *
-     * @return The recipe registry
+     * @return The crafting recipe registry
      */
-    RecipeRegistry getRecipeRegistry();
+    RecipeRegistry<CraftingRecipe> getCraftingRecipeRegistry();
+
+    /**
+     * Retrieves the smelting RecipeRegistry for this GameRegistry.
+     *
+     * @return The smelting recipe registry
+     */
+    RecipeRegistry<SmeltingRecipe> getSmeltingRecipeRegistry();
 
     /**
      * Gets a {@link ResourcePack} that's already been created by its ID.
