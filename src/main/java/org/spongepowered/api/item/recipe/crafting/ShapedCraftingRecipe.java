7c1bc39ab030450dea2e443be830c7460925ac5f
Jakub Hlusička <limeth@protonmail.com>
2017-01-03 18:08:29 +0100

    move unnecessary implementation to SpongeCommon

--- src/main/java/org/spongepowered/api/item/recipe/crafting/ShapedCraftingRecipe.java
+++ src/main/java/org/spongepowered/api/item/recipe/crafting/ShapedCraftingRecipe.java
@@ -25,13 +25,9 @@
 package org.spongepowered.api.item.recipe.crafting;
 
 import org.spongepowered.api.Sponge;
-import org.spongepowered.api.item.ItemTypes;
 import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.api.item.inventory.ItemStackSnapshot;
-import org.spongepowered.api.item.inventory.Slot;
-import org.spongepowered.api.item.inventory.type.GridInventory;
 import org.spongepowered.api.util.ResettableBuilder;
-import org.spongepowered.api.world.World;
 
 import java.util.List;
 import java.util.Map;
@@ -67,77 +63,14 @@ public interface ShapedCraftingRecipe extends CraftingRecipe {
      * @param symbol The symbol paired with the ingredient predicate in the aisle
      * @return The paired ingredient predicate
      */
-    default Optional<Predicate<ItemStackSnapshot>> getIngredientPredicate(char symbol) {
-        return Optional.ofNullable(getIngredientPredicates().get(symbol));
-    }
+    Optional<Predicate<ItemStackSnapshot>> getIngredientPredicate(char symbol);
 
     /**
      * @param x The x coordinate counted from the left side
      * @param y The y coordinate counted from the top
      * @return The ingredient predicate at this position defined by the aisle
      */
-    default Optional<Predicate<ItemStackSnapshot>> getIngredientPredicate(int x, int y) {
-        if (x < 0 || x >= getWidth() || y < 0 || y >= getHeight())
-            return Optional.empty();
-
-        char symbol = getAisle().get(y).charAt(x);
-
-        return getIngredientPredicate(symbol);
-    }
-
-    @Override
-    default boolean isValid(GridInventory grid, World world) {
-        int gapWidth = grid.getColumns() - getWidth();
-        int gapHeight = grid.getRows() - getHeight();
-
-        // Shift the aisle along the grid wherever possible
-        for (int offsetX = 0; offsetX <= gapWidth; offsetX++) {
-            byShiftingTheAisle:
-            for (int offsetY = 0; offsetY <= gapHeight; offsetY++) {
-                // Test each predicate in the aisle
-                for (int aisleX = 0; aisleX < getWidth(); aisleX++) {
-                    for (int aisleY = 0; aisleY < getHeight(); aisleY++) {
-                        final int finalAisleX = aisleX;
-                        final int finalAisleY = aisleY;
-                        int gridX = aisleX + offsetX;
-                        int gridY = aisleY + offsetY;
-                        Slot slot = grid.getSlot(gridX, gridY)
-                                .orElseThrow(() -> new IllegalStateException("Could not access the slot," +
-                                " even though it was supposed to be in bounds."));
-                        ItemStack itemStack = slot.peek()
-                                .orElseThrow(() -> new IllegalStateException("Could not access the ItemStack" +
-                                " in the slot as it's currently unavailable."));
-                        Predicate<ItemStackSnapshot> ingredientPredicate = getIngredientPredicate(aisleX, aisleY)
-                                .orElseThrow(() -> new IllegalStateException("The ingredient predicate at [" +
-                                        finalAisleX + "; " + finalAisleY + "] is not present. There should be" +
-                                        " one for each symbol in the aisle!"));
-
-                        if (!ingredientPredicate.test(itemStack.createSnapshot()))
-                            continue byShiftingTheAisle;
-                    }
-                }
-
-                // Make sure the gap is empty
-                for (int gapX = 0; gapX < gapWidth; gapX++) {
-                    for (int gapY = 0; gapY < gapHeight; gapY++) {
-                        int gridX = gapX + (gapX >= offsetX ? getWidth() : 0);
-                        int gridY = gapY + (gapY >= offsetY ? getHeight() : 0);
-                        boolean empty = grid.getSlot(gridX, gridY)
-                                .flatMap(Slot::peek)
-                                .map(itemStack -> itemStack.getItem() == ItemTypes.NONE)
-                                .orElse(true);
-
-                        if (!empty)
-                            continue byShiftingTheAisle;
-                    }
-                }
-
-                return true;
-            }
-        }
-
-        return false;
-    }
+    Optional<Predicate<ItemStackSnapshot>> getIngredientPredicate(int x, int y);
 
     /**
      * Gets the width of the grid this ShapedCraftingRecipe fits into.
