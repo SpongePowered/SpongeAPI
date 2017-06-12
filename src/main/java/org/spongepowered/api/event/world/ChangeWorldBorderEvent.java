80e588f0b94c74f6663ad1190eb907822a12055a
sanman00 <sanjaywygold2011@hotmail.co.uk>
2017-06-12 10:08:30 +0100

    Add support for player world borders (#1521)
    * Squash commits are below:
    
    Add API support for getting per-player WorldBorders
    
    Add the event for world border changing
    
    Remove pointless files
    
    Change import order
    
    Make sub-event interfaces non-static
    
    Add setter for the player's world border
    
    * Add a setter for a World's border for consistency with Player's methods
    
    Move sub-event interfaces to be after the event methods
    
    Require event cause when setting borders
    
    Clarify contract for getWorldBorder() and add default-implemented ease of use methods
    
    Modify getWorldBorder() contract slightly
    
    Remove convenience methods, clarify Javadocs and make added event extend Event
    
    Add method for resetting the player's world border
    
    Allow players to not have a world border set
    
    * Clarify Javadocs for the case when no border is set
    
    Add builder for WorldBorder
    
    Add a Javadoc to the builder() method
    
    Remove full stops from at-clauses in Javadocs
    
    * Remove unneeded method from Player
    
    Add @Nullable annotation to world border setter
    
    Add method to WorldBorder that copies properties from another border
    
    * Remove the equivalent method from World as the name was misleading
    
    Change name of property-copying method
    
    Explain when IllegalStateException would be thrown by builder
    
    Fix typo in Javadocs
    
    Add Javadocs to subevents
    
    Make builder no longer throw an exception if no data is given
    
    Fix indentation in @return tag

---
+++ src/main/java/org/spongepowered/api/event/world/ChangeWorldBorderEvent.java
@@ -0,0 +1,74 @@
+/*
+ * This file is part of SpongeAPI, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.api.event.world;
+
+import org.spongepowered.api.event.Cancellable;
+import org.spongepowered.api.event.Event;
+import org.spongepowered.api.event.entity.living.humanoid.player.TargetPlayerEvent;
+import org.spongepowered.api.util.annotation.eventgen.GenerateFactoryMethod;
+import org.spongepowered.api.world.WorldBorder;
+
+import java.util.Optional;
+
+/**
+ * Fired when the world border changes for a {@code Player} or a {@code World}.
+ */
+@GenerateFactoryMethod
+public interface ChangeWorldBorderEvent extends Event, Cancellable {
+    /**
+     * Gets the border that was previously used as an {@link Optional}. There 
+     * may be no border set for the target (especially, if it is a player), in 
+     * which case, an empty {@code Optional} is returned.
+     *
+     * @return The border that was previously in use, if there was one.
+     */
+    Optional<WorldBorder> getPreviousBorder();
+
+    /**
+     * Gets the border that will be used as an {@link Optional}. For some
+     * targets, the border may be unset and therefore they may not always have a
+     * border. It is in these instances that an empty {@code Optional} is
+     * returned.
+     *
+     * @return The border that will be in use, if there is one.
+     */
+    Optional<WorldBorder> getNewBorder();
+
+    /**
+     * An event that is fired when a world's border is changed.
+     */
+    interface TargetWorld extends ChangeWorldBorderEvent, TargetWorldEvent {
+
+    }
+
+    /**
+     * An event that is fired when a player's world border is changed. This may 
+     * also be fired when a player's border is set or unset.
+     */
+    interface TargetPlayer extends ChangeWorldBorderEvent, TargetPlayerEvent {
+
+    }
+
+}
