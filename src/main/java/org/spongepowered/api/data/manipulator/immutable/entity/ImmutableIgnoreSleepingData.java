19db1dc122711baae255e9081622b3086b2423c3
sanman00 <sanjaywygold2011@hotmail.co.uk>
2017-08-22 17:55:18 +0100

    Add another data manipulator to handle ignoreSleeping()

--- src/main/java/org/spongepowered/api/data/manipulator/immutable/entity/ImmutableIgnoreSleepingData.java
+++ src/main/java/org/spongepowered/api/data/manipulator/immutable/entity/ImmutableIgnoreSleepingData.java
@@ -22,28 +22,24 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package org.spongepowered.api.data.manipulator.mutable.entity;
+package org.spongepowered.api.data.manipulator.immutable.entity;
 
 import org.spongepowered.api.data.key.Keys;
-import org.spongepowered.api.data.manipulator.DataManipulator;
-import org.spongepowered.api.data.manipulator.immutable.entity.ImmutableSleepingData;
-import org.spongepowered.api.data.value.mutable.Value;
-import org.spongepowered.api.entity.living.Bat;
+import org.spongepowered.api.data.manipulator.ImmutableDataManipulator;
+import org.spongepowered.api.data.manipulator.mutable.entity.IgnoreSleepingData;
+import org.spongepowered.api.data.value.immutable.ImmutableValue;
 import org.spongepowered.api.entity.living.player.Player;
 
-/**
- * An {@link DataManipulator} for the "sleeping" state of a
- * {@link Player}. Also applicable to {@link Bat}s where they can be
- * shown as "sleeping".
- */
-public interface SleepingData extends DataManipulator<SleepingData, ImmutableSleepingData> {
+public interface ImmutableIgnoreSleepingData extends ImmutableDataManipulator<ImmutableIgnoreSleepingData, IgnoreSleepingData> {
 
     /**
-     * Gets the {@link Value} for the "sleeping" state.
+     * Gets the {@link ImmutableValue} for whether the owning {@link Player}'s
+     * "sleeping" state is ignored when vanilla mechanics dictate that all
+     * {@link Player}s must be sleeping to advance through the night.
      *
-     * @return The mutable value for the "sleeping" state
-     * @see Keys#IS_SLEEPING
+     * @return The immutable value for ignoring sleep
+     * @see Keys#IGNORE_SLEEPING
      */
-    Value<Boolean> sleeping();
+    ImmutableValue<Boolean> ignoreSleeping();
 
 }
