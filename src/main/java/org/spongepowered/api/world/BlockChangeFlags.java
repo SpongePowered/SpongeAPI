bcb96610b3e9aeb40f819c3352e08cdadb9084b3
Seppe Volkaerts <seppevolkaerts@hotmail.com>
2017-12-19 18:08:06 +0100

    Added docs to the cataloged block change flags.

--- src/main/java/org/spongepowered/api/world/BlockChangeFlags.java
+++ src/main/java/org/spongepowered/api/world/BlockChangeFlags.java
@@ -24,31 +24,69 @@
  */
 package org.spongepowered.api.world;
 
+import org.spongepowered.api.block.BlockTypes;
 import org.spongepowered.api.util.generator.dummy.DummyObjectProvider;
 
 /**
  * An enumeration of the possible {@link BlockChangeFlag}s.
+ * <p>
+ * {@link BlockChangeFlag}s can be combined using the
+ * {@link BlockChangeFlag#andFlag(BlockChangeFlag)} and
+ * {@link BlockChangeFlag#andNotFlag(BlockChangeFlag)} operators.
  */
 public final class BlockChangeFlags {
 
     // SORTFIELDS:ON
 
+    /**
+     * All the available flags are applied through the AND operator.
+     */
     public static final BlockChangeFlag ALL = DummyObjectProvider.createFor(BlockChangeFlag.class, "ALL");
 
+    /**
+     * A flag that defines whether a block change should notify neighboring blocks.
+     */
     public static final BlockChangeFlag NEIGHBOR = DummyObjectProvider.createFor(BlockChangeFlag.class, "NEIGHBOR");
 
+    /**
+     * The {@link #NEIGHBOR} and {@link #OBSERVER} flags combined with the AND operator.
+     */
     public static final BlockChangeFlag NEIGHBOR_OBSERVER = DummyObjectProvider.createFor(BlockChangeFlag.class, "NEIGHBOR_OBSERVER");
 
+    /**
+     * The {@link #NEIGHBOR} and {@link #PHYSICS} flags combined with the AND operator.
+     */
     public static final BlockChangeFlag NEIGHBOR_PHYSICS = DummyObjectProvider.createFor(BlockChangeFlag.class, "NEIGHBOR_PHSYICS");
 
+    /**
+     * The {@link #NEIGHBOR}, {@link #PHYSICS} and {@link #OBSERVER} flags combined with the AND operator.
+     */
     public static final BlockChangeFlag NEIGHBOR_PHYSICS_OBSERVER = DummyObjectProvider.createFor(BlockChangeFlag.class, "NEIGHBOR_PHYSICS_OBSERVER");
 
+    /**
+     * No flags are set, triggers nothing.
+     */
     public static final BlockChangeFlag NONE = DummyObjectProvider.createFor(BlockChangeFlag.class, "NONE");
 
+    /**
+     * A flag that defines whether to update observer blocks, different
+     * from notifying neighbors in that neighbor notifications
+     * can cause further block notification loops (like redstone),
+     * whereas this focuses on {@link BlockTypes#OBSERVER} blocks
+     * being told of updates.
+     */
     public static final BlockChangeFlag OBSERVER = DummyObjectProvider.createFor(BlockChangeFlag.class, "OBSERVER");
 
+    /**
+     * A flag that defines whether a block change should
+     * perform block physics checks or not. If not, no checks
+     * are performed.
+     */
     public static final BlockChangeFlag PHYSICS = DummyObjectProvider.createFor(BlockChangeFlag.class, "PHYSICS");
 
+    /**
+     * The {@link #PHYSICS} and {@link #OBSERVER} flags combined with the AND operator.
+     */
     public static final BlockChangeFlag PHYSICS_OBSERVER = DummyObjectProvider.createFor(BlockChangeFlag.class, "PHYSICS_OBSERVER");
 
     // SORTFIELDS:OFF
