fff8013557684a7190d6677cd99ab4d0ef43f74e
RedNesto <RedNestoMC@gmail.com>
2018-02-04 20:56:38 +0100

    Refactoring of LaunchProjectileEvent - Removal of TargetProjectileEvent

--- src/main/java/org/spongepowered/api/event/entity/projectile/LaunchProjectileEvent.java
+++ src/main/java/org/spongepowered/api/event/entity/projectile/LaunchProjectileEvent.java
@@ -24,12 +24,31 @@
  */
 package org.spongepowered.api.event.entity.projectile;
 
+import org.spongepowered.api.entity.Entity;
 import org.spongepowered.api.entity.projectile.Projectile;
-import org.spongepowered.api.event.Cancellable;
+import org.spongepowered.api.event.entity.AffectEntityEvent;
+import org.spongepowered.api.event.entity.ConstructEntityEvent;
+import org.spongepowered.api.event.entity.SpawnEntityEvent;
+import org.spongepowered.api.util.annotation.eventgen.PropertySettings;
+
+import java.util.List;
 
 /**
- * Called when a {@link Projectile} is launched.
+ * Called when a {@link Projectile} is launched. This event is always followed by a {@link SpawnEntityEvent}
+ * and preceded by a {@link ConstructEntityEvent}.
  */
-public interface LaunchProjectileEvent extends TargetProjectileEvent, Cancellable {
+public interface LaunchProjectileEvent extends AffectEntityEvent {
 
+    @Override
+    @PropertySettings(requiredParameter = false, generateMethods = false)
+    default List<Entity> getEntities() {
+        return (List<Entity>) (List<?>) getProjectiles();
+    }
+
+    /**
+     * Returns a modifiable list of the projectiles handled by this event.
+     *
+     * @return a modifiable list of projectiles.
+     */
+    List<Projectile> getProjectiles();
 }
