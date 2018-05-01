4566330a2263675c060b1b327eca972d04785436
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2018-05-01 08:10:08 +0200

    Better ChangeEntityEquipmentEvents

--- src/main/java/org/spongepowered/api/event/entity/ChangeEntityEquipmentEvent.java
+++ src/main/java/org/spongepowered/api/event/entity/ChangeEntityEquipmentEvent.java
@@ -26,70 +26,30 @@ package org.spongepowered.api.event.entity;
 
 import org.spongepowered.api.data.Transaction;
 import org.spongepowered.api.entity.Entity;
-import org.spongepowered.api.entity.living.Humanoid;
-import org.spongepowered.api.entity.living.Living;
-import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Cancellable;
-import org.spongepowered.api.event.entity.living.TargetLivingEvent;
-import org.spongepowered.api.event.entity.living.humanoid.TargetHumanoidEvent;
-import org.spongepowered.api.event.entity.living.humanoid.player.TargetPlayerEvent;
 import org.spongepowered.api.event.item.inventory.TargetInventoryEvent;
 import org.spongepowered.api.item.inventory.Inventory;
 import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.api.item.inventory.ItemStackSnapshot;
 import org.spongepowered.api.item.inventory.Slot;
-import org.spongepowered.api.util.annotation.eventgen.GenerateFactoryMethod;
-
-import java.util.Optional;
 
 /**
  * Called when an entity changes an equipped item. This can occur whenever
  * a {@link Slot} belonging to an {@link Inventory} of an {@link Entity}
  * is filled with an {@link ItemStack}, emptied of an {@link ItemStack},
- * or swapped with an {@link ItemStack}. The requirement of course is
- * that if the {@link #getOriginalItemStack()} is {@link Optional#empty()}, then
- * the {@link #getItemStack()} must be present, and vice versa. In the event
- * that a change to the suggested {@link ItemStack}, the use of the
- * {@link Transaction} is recommended.
+ * or swapped with an {@link ItemStack}. In the event that a change to the
+ * {@link ItemStack}, the use of the {@link Transaction} is recommended.
  */
-@GenerateFactoryMethod
 public interface ChangeEntityEquipmentEvent extends TargetEntityEvent, TargetInventoryEvent, Cancellable {
 
     /**
-     * Gets the previously equipped {@link ItemStack} as an
-     * {@link ItemStackSnapshot}.
+     * Gets the {@link Transaction} of {@link ItemStackSnapshot}s for this event.
      *
-     * <p>The previously equipped item may have been empty.</p>
-     *
-     * @return The original itemstack, if available
+     * @return The transaction of the item
      */
-    Optional<ItemStackSnapshot> getOriginalItemStack();
-
-    /**
-     * Gets the new {@link ItemStack} that is being equipped in the relative
-     * armor slot.
-     *
-     * <p>The itemstack may not exist or the slot is being emptied.</p>
-     *
-     * @return The new item stack, if available
-     */
-    Optional<Transaction<ItemStackSnapshot>> getItemStack();
+    Transaction<ItemStackSnapshot> getTransaction();
 
     @Override
     Slot getTargetInventory();
 
-    /**
-     * An event where a {@link Living} entity is targeted.
-     */
-    interface TargetLiving extends ChangeEntityEquipmentEvent, TargetLivingEvent { }
-
-    /**
-     * An event where a {@link Humanoid} entity is targeted.
-     */
-    interface TargetHumanoid extends TargetLiving, TargetHumanoidEvent { }
-
-    /**
-     * An event where a {@link Player} entity is targeted.
-     */
-    interface TargetPlayer extends TargetHumanoid, TargetPlayerEvent { }
 }
