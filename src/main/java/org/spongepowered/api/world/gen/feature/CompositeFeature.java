9190d71609f633fec1da4455c7d9c634879fbf86
Chris Sanders <zidane@spongepowered.org>
2019-02-14 11:38:53 -0600

    Do this before Aaron brings the pitchfork.
    Signed-off-by: Chris Sanders <zidane@spongepowered.org>

--- src/main/java/org/spongepowered/api/world/gen/feature/CompositeFeature.java
+++ src/main/java/org/spongepowered/api/world/gen/feature/CompositeFeature.java
@@ -49,6 +49,6 @@ public interface CompositeFeature<F extends FeatureConfig, P extends PlacementCo
 
     @Override
     default boolean generate(ProtoWorld<?> world, TerrainGenerator<?> generator, Random random, Vector3i origin, F config) {
-        return this.getPlacer().place(world, generator, random, origin, this.getPlacementConfig(), this.getFeature(), config)
+        return this.getPlacer().place(world, generator, random, origin, this.getPlacementConfig(), this.getFeature(), config);
     }
 }
