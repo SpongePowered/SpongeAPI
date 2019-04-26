881d7e9d8f6c54bee1be2a1e62a977325347d6e8
Seppe Volkaerts <seppevolkaerts@hotmail.com>
2019-04-25 23:04:44 -0500

    Move the BlockState matcher impl to impl and outside of the BlockState interface.

--- src/main/java/org/spongepowered/api/block/BlockState.java
+++ src/main/java/org/spongepowered/api/block/BlockState.java
@@ -24,13 +24,6 @@
  */
 package org.spongepowered.api.block;
 
-import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.base.Preconditions.checkNotNull;
-import static com.google.common.base.Preconditions.checkState;
-
-import com.google.common.base.MoreObjects;
-import com.google.common.base.Objects;
-import com.google.common.collect.ImmutableList;
 import org.spongepowered.api.CatalogType;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.block.tileentity.TileEntity;
@@ -44,18 +37,12 @@ import org.spongepowered.api.data.value.BaseValue;
 import org.spongepowered.api.data.value.immutable.ImmutableValue;
 import org.spongepowered.api.data.value.mutable.Value;
 import org.spongepowered.api.util.Cycleable;
-import org.spongepowered.api.util.ResettableBuilder;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.function.Predicate;
-
-import javax.annotation.Nullable;
 
 /**
  * Represents a particular "state" that can exist at a {@link Location} with
@@ -76,13 +63,13 @@ public interface BlockState extends ImmutableDataHolder<BlockState>, DirectionRe
     }
 
     /**
-     * Constructs a new {@link MatcherBuilder} to construct a {@link StateMatcher}.
+     * Constructs a new builder to construct a {@link BlockStateMatcher}.
      *
      * @param type The block type
      * @return The builder
      */
-    static MatcherBuilder matcher(BlockType type) {
-        return new MatcherBuilder().type(type);
+    static BlockStateMatcher.Builder matcher(BlockType type) {
+        return BlockStateMatcher.builder().type(type);
     }
 
     /**
@@ -225,193 +212,5 @@ public interface BlockState extends ImmutableDataHolder<BlockState>, DirectionRe
          */
         Builder blockType(BlockType blockType);
 
-    }
-
-    /**
-     * A builder for building {@link StateMatcher}s.
-     */
-    final class MatcherBuilder implements ResettableBuilder<StateMatcher, MatcherBuilder> {
-
-        @Nullable private BlockType type;
-        private ArrayList<BlockTrait<?>> traits = new ArrayList<>();
-        private ArrayList<Object> values = new ArrayList<>();
-
-        private MatcherBuilder() {
-        }
-
-        /**
-         * Sets the root {@link BlockType} for the {@link StateMatcher}.
-         * <p>Note that the {@link BlockType type} <b>must be set prior</b>
-         * to setting various {@link BlockTrait traits} and their values.</p>
-         *
-         * @param type The block type to use
-         * @return This builder, for chaining
-         */
-        public MatcherBuilder type(BlockType type) {
-            this.type = checkNotNull(type, "BlockType cannot be null!");
-            return this;
-        }
-
-        /**
-         * Adds the desired {@link BlockTrait} and {code value} to this
-         * builder, if the desired {@link BlockTrait} does not belong to the
-         * original {@link BlockType} as provided by {@link #type(BlockType)},
-         * an exception is thrown. Likewise, if a {@code value} is not within
-         * the possible values for the desired trait of the desired type, an
-         * exception is thrown.
-         *
-         * @param trait The desired block trait
-         * @param value the desired value
-         * @param <T> The type of comparable
-         * @return This builder
-         * @throws IllegalArgumentException If the block trait does not match
-         *     the block type, or if the value does not belong to the trait
-         *     with the desired block type
-         */
-        public <T extends Comparable<T>> MatcherBuilder trait(BlockTrait<T> trait, T value) throws IllegalArgumentException {
-            checkState(this.type != null, "BlockType cannot be null! Must be set before using any traits!");
-            checkArgument(this.type.getTraits().contains(trait), "BlockType does not contain the specified trait: %s", trait);
-            checkArgument(trait.getPossibleValues().contains(value), "BlockTrait %s does not contain value %s", trait, value);
-            checkArgument(!this.traits.contains(trait), "Already contains the trait %s! Cannot add multiple values!", trait);
-            this.traits.add(trait);
-            this.values.add(value);
-            return this;
-        }
-
-        /**
-         * Creates a new {@link StateMatcher}.
-         *
-         * @return The new state matcher
-         * @throws IllegalStateException If there is no block type
-         */
-        public StateMatcher build() throws IllegalStateException {
-            checkState(this.type != null, "BlockType cannot be null!");
-            return new StateMatcher(this.type, this.traits.toArray(new BlockTrait<?>[0]), this.values.toArray());
-
-        }
-
-        @SuppressWarnings({"unchecked", "rawtypes"})
-        @Override
-        public MatcherBuilder from(StateMatcher value) {
-            reset();
-            type(value.type);
-            for (int i = 0; i < value.traits.length; i++) {
-                trait((BlockTrait) value.traits[i], (Comparable) value.values[i]);
-            }
-            return this;
-        }
-
-        @Override
-        public MatcherBuilder reset() {
-            this.type = null;
-            this.traits.clear();
-            this.values.clear();
-            return this;
-        }
-    }
-
-    /**
-     * A {@link BlockState} matcher that will match various block states
-     * according to a pre-built list of {@link BlockTrait}s and their
-     * values, such that not all {@link BlockTrait}s contained in a
-     * {@link BlockState} must be matched. (Such as if a block state
-     * that contains 4 traits, and only 2 are wanting to be matched,
-     * then the other two traits may be variable).
-     */
-    final class StateMatcher implements Predicate<BlockState> {
-        private final BlockType type;
-        private final BlockTrait<?>[] traits;
-        private final Object[] values;
-        @Nullable private ImmutableList<BlockState> compatibleStates; // Lazily constructed
-
-        private StateMatcher(BlockType type, BlockTrait<?>[] traits, Object[] values) {
-            this.type = type;
-            this.traits = new BlockTrait<?>[traits.length];
-            System.arraycopy(traits, 0, this.traits, 0, traits.length);
-            this.values = new Object[values.length];
-            System.arraycopy(values, 0, this.values, 0, values.length);
-        }
-
-        private ImmutableList<BlockState> computeCompatibleStates() {
-            return this.type.getAllBlockStates()
-                    .stream()
-                    .filter(this::matches)
-                    .collect(ImmutableList.toImmutableList());
-        }
-
-        /**
-         * Gets a {@code true} return value if the provided {@link BlockState}
-         * sufficiently matches this matcher, such that the {@link BlockType}
-         * matches, and the pre-defined {@link BlockTrait} values match.
-         *
-         * @param state The block state in question
-         * @return True if the state sufficiently matches
-         */
-        public boolean matches(BlockState state) {
-            if (this.type != state.getType()) {
-                return false;
-            }
-            for (int i = 0; i < this.traits.length; i++) {
-                final BlockTrait<?> trait = this.traits[i];
-                final Object value = this.values[i];
-                final Optional<?> traitValue = state.getTraitValue(trait);
-                if (!traitValue.isPresent()) { // If for any reason this fails, that means there's another problem, but alas, just in case
-                    return false;
-                }
-                final Object o = traitValue.get();
-                if (!value.equals(o)) {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        @Override
-        public boolean test(BlockState blockState) {
-            return matches(blockState);
-        }
-
-        /**
-         * Gets a {@link List} of compatible {@link BlockState}s.
-         * Since all {@link BlockState}s are known in the initialization
-         * of a {@link BlockType}, the states are already deterministic
-         * and cannot change themselves.
-         *
-         * @return The list of compatible block states
-         */
-        public List<BlockState> getCompatibleStates() {
-            if (this.compatibleStates == null) {
-                this.compatibleStates = computeCompatibleStates();
-            }
-            return this.compatibleStates;
-        }
-
-        @Override
-        public String toString() {
-            return MoreObjects.toStringHelper(this)
-                    .add("type", this.type)
-                    .add("traits", this.traits)
-                    .add("values", this.values)
-                    .toString();
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            StateMatcher that = (StateMatcher) o;
-            return Objects.equal(this.type, that.type)
-                   && Objects.equal(this.traits, that.traits)
-                   && Objects.equal(this.values, that.values);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hashCode(this.type, this.traits, this.values);
-        }
     }
 }
---
+++ src/main/java/org/spongepowered/api/block/BlockStateMatcher.java
@@ -0,0 +1,120 @@
+/*
+ * This file is part of SpongeAPI, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.api.block;
+
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.block.trait.BlockTrait;
+import org.spongepowered.api.util.ResettableBuilder;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * A {@link BlockState} matcher that will match various block states
+ * according to a pre-built list of {@link BlockTrait}s and their
+ * values, such that not all {@link BlockTrait}s contained in a
+ * {@link BlockState} must be matched. (Such as if a block state
+ * that contains 4 traits, and only 2 are wanting to be matched,
+ * then the other two traits may be variable).
+ */
+public interface BlockStateMatcher extends Predicate<BlockState> {
+
+    /**
+     * Constructs a new {@link Builder} to construct a {@link BlockStateMatcher}.
+     *
+     * @return The builder
+     */
+    static Builder builder() {
+        return Sponge.getRegistry().createBuilder(Builder.class);
+    }
+
+    /**
+     * Gets a {@code true} return value if the provided {@link BlockState}
+     * sufficiently matches this matcher, such that the {@link BlockType}
+     * matches, and the pre-defined {@link BlockTrait} values match.
+     *
+     * @param state The block state in question
+     * @return True if the state sufficiently matches
+     */
+    boolean matches(BlockState state);
+
+    @Override
+    default boolean test(BlockState blockState) {
+        return matches(blockState);
+    }
+
+    /**
+     * Gets a {@link List} of compatible {@link BlockState}s.
+     * Since all {@link BlockState}s are known in the initialization
+     * of a {@link BlockType}, the states are already deterministic
+     * and cannot change themselves.
+     *
+     * @return The list of compatible block states
+     */
+    List<BlockState> getCompatibleStates();
+
+    /**
+     * A builder for building {@link BlockStateMatcher}s.
+     */
+    interface Builder extends ResettableBuilder<BlockStateMatcher, Builder> {
+
+        /**
+         * Sets the root {@link BlockType} for the {@link BlockStateMatcher}.
+         * <p>Note that the {@link BlockType type} <b>must be set prior</b>
+         * to setting various {@link BlockTrait traits} and their values.</p>
+         *
+         * @param type The block type to use
+         * @return This builder, for chaining
+         */
+        Builder type(BlockType type);
+
+        /**
+         * Adds the desired {@link BlockTrait} and {code value} to this
+         * builder, if the desired {@link BlockTrait} does not belong to the
+         * original {@link BlockType} as provided by {@link #type(BlockType)},
+         * an exception is thrown. Likewise, if a {@code value} is not within
+         * the possible values for the desired trait of the desired type, an
+         * exception is thrown.
+         *
+         * @param trait The desired block trait
+         * @param value the desired value
+         * @param <T> The type of comparable
+         * @return This builder
+         * @throws IllegalArgumentException If the block trait does not match
+         *     the block type, or if the value does not belong to the trait
+         *     with the desired block type
+         */
+        <T extends Comparable<T>> Builder trait(BlockTrait<T> trait, T value) throws IllegalArgumentException;
+
+        /**
+         * Creates a new {@link BlockStateMatcher}.
+         *
+         * @return The new state matcher
+         * @throws IllegalStateException If there is no block type
+         */
+        BlockStateMatcher build() throws IllegalStateException;
+
+    }
+}
