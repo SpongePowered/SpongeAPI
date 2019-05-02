ab93ab2c84eebc426fc8f58ffee272858d3aee59
Chris Sanders <zidane@spongepowered.org>
2019-05-02 13:50:09 -0500

    Transform moves to the implementation.
    Signed-off-by: Chris Sanders <zidane@spongepowered.org>

--- src/main/java/org/spongepowered/api/entity/Transform.java
+++ src/main/java/org/spongepowered/api/entity/Transform.java
@@ -24,16 +24,12 @@
  */
 package org.spongepowered.api.entity;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
 import com.flowpowered.math.imaginary.Quaterniond;
 import com.flowpowered.math.matrix.Matrix4d;
 import com.flowpowered.math.vector.Vector3d;
-import com.google.common.base.MoreObjects;
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
-
-import javax.annotation.Nullable;
 
 /**
  * Represents the immutable world attributes of an {@link Entity}. Comprised of
@@ -54,84 +50,43 @@ import javax.annotation.Nullable;
  * of the transform in case it gets added later. For now this return
  * {@link Vector3d#ONE}.</p>
  */
-public final class Transform {
+public interface Transform {
 
-    private final World world;
-    private final Vector3d position;
-    private final Vector3d rotation;
-    private final Vector3d scale;
-    @Nullable private Location location = null;
-    @Nullable private Quaterniond rotationQuaternion = null;
+    static Transform of(World world) {
+        return Sponge.getRegistry().requireFactory(Factory.class).create(Location.of(world, Vector3d.ZERO), Vector3d.ZERO, Vector3d.ONE);
+    }
 
-    /**
-     * Creates a new {@link Transform} based on the provided {@link Location}.
-     *
-     * @param location The provided location that provides the
-     *     world, and position
-     */
-    public Transform(Location location) {
-        this(location.getWorld(), location.getPosition());
+    static Transform of(Location location) {
+        return Sponge.getRegistry().requireFactory(Factory.class).create(location, Vector3d.ZERO, Vector3d.ONE);
+    }
+
+    static Transform of(World world, Vector3d position) {
+        return Sponge.getRegistry().requireFactory(Factory.class).create(Location.of(world, position), Vector3d.ZERO, Vector3d.ONE);
+    }
+
+    static Transform of(World world, Vector3d position, Vector3d rotation) {
+        return Sponge.getRegistry().requireFactory(Factory.class).create(Location.of(world, position), rotation, Vector3d.ONE);
+    }
+
+    static Transform of(Location location, Vector3d rotation, Vector3d scale) {
+        return Sponge.getRegistry().requireFactory(Factory.class).create(location, rotation, scale);
     }
 
     /**
-     * Creates a new {@link Transform} with the provided {@link World world}.
-     * The default position is {@link Vector3d#ZERO}.
+     * Returns if this {@link Transform} is still valid.
      *
-     * @param world The world to use
-     */
-    public Transform(World world) {
-        this(world, Vector3d.ZERO);
-    }
-
-    /**
-     * Creates a new {@link Transform} with the provided {@link World world}
-     * and {@link Vector3d position}.
+     * <p>Examples of invalid Transforms are:</p>
      *
-     * @param world The world to use
-     * @param position The position to use
-     */
-    public Transform(World world, Vector3d position) {
-        this(world, position, Vector3d.ZERO);
-    }
-
-    /**
-     * Creates a new {@link Transform} with the provided {@link World world},
-     * {@link Vector3d position}, and {@link Vector3d rotation}.
+     * <ul>
+     *     <li>A Transform without an {@link World}</li>
+     *     <li>A Transform whose {@link World} object is no longer present</li>
+     *     <li>A Transform whose coordinates are illegal (defined by the
+     *     implementation)</li>
+     * </ul>
      *
-     * @param world The world to use
-     * @param position The position to use
-     * @param rotation The rotation to use
+     * @return True if valid, false if not
      */
-    public Transform(World world, Vector3d position, Vector3d rotation) {
-        this(world, position, rotation, Vector3d.ONE);
-    }
-
-    /**
-     * Creates a new {@link Transform} with the provided {@link Location},
-     * {@link Vector3d rotation}, and {@link Vector3d scale}.
-     *
-     * @param location The location to use, providing the world and position
-     * @param rotation The rotation to use
-     * @param scale The scale to use
-     */
-    public Transform(Location location, Vector3d rotation, Vector3d scale) {
-        this(location.getWorld(), location.getPosition(), rotation, scale);
-    }
-
-    /**
-     * Creates a new {@link Transform}.
-     *
-     * @param world The world to use
-     * @param position The position to use
-     * @param rotation The rotation to use
-     * @param scale The scale to use
-     */
-    public Transform(World world, Vector3d position, Vector3d rotation, Vector3d scale) {
-        this.world = checkNotNull(world, "world");
-        this.position = checkNotNull(position, "position");
-        this.rotation = checkNotNull(rotation, "rotation");
-        this.scale = checkNotNull(scale, "scale");
-    }
+    boolean isValid();
 
     /**
      * Gets the {@link Location} this transform contains. This is the position
@@ -140,37 +95,23 @@ public final class Transform {
      * @return The location
      * @throws IllegalStateException If the transform doesn't have a world
      */
-    public Location getLocation() {
-        if (this.location == null) {
-            this.location = Location.of(this.world, this.position);
-        }
-        return this.location;
-    }
+    Location getLocation();
 
     /**
-     * Creates a copy of this transform and sets the {@link Location}. This sets
-     * both the position and the world.
+     * Creates a copy of this transform and sets the {@link Location}.
      *
      * @param location The new location
      * @return A new transform
      */
-    public Transform setLocation(Location location) {
-        checkNotNull(location, "location");
-        return new Transform(location, getRotation(), getScale());
-    }
+    Transform withLocation(Location location);
 
     /**
-     * Gets the {@link World} this transform contains.
-     *
-     * <p>Note: This can be null if the {@link World} is unloaded and garbage
-     * collected.</p>
+     * Gets the {@link World}.
      *
      * @return The world
      * @throws IllegalStateException If the transform doesn't have a world
      */
-    public World getWorld() {
-        return this.world;
-    }
+    World getWorld();
 
     /**
      * Creates a copy of this transform and sets the {@link World}.
@@ -178,19 +119,14 @@ public final class Transform {
      * @param world The new world
      * @return A new transform
      */
-    public Transform setWorld(World world) {
-        checkNotNull(world, "world");
-        return new Transform(world, getPosition(), getRotation(), getScale());
-    }
+    Transform withWorld(World world);
 
     /**
      * Gets the coordinates of this transform.
      *
      * @return The coordinates
      */
-    public Vector3d getPosition() {
-        return this.position;
-    }
+    Vector3d getPosition();
 
     /**
      * Creates a copy of this transform while setting the position of the new
@@ -199,10 +135,7 @@ public final class Transform {
      * @param position The position
      * @return A new transform
      */
-    public Transform setPosition(Vector3d position) {
-        checkNotNull(position, "position");
-        return new Transform(getWorld(), position, getRotation(), getScale());
-    }
+    Transform withPosition(Vector3d position);
 
     /**
      * Gets the rotation of this transform, as a {@link Vector3d}.
@@ -216,9 +149,18 @@ public final class Transform {
      *
      * @return The rotation vector
      */
-    public Vector3d getRotation() {
-        return this.rotation;
-    }
+    Vector3d getRotation();
+
+    /**
+     * Creates a copy of this transform and sets the rotation as a quaternion.
+     *
+     * <p>Quaternions are objectively better than the Euler angles preferred by
+     * Minecraft. This is for compatibility with the flow-math library.</p>
+     *
+     * @param rotation The new rotation
+     * @return A new transform
+     */
+    Transform withRotation(Vector3d rotation);
 
     /**
      * Creates a copy of this transform and sets the rotation.
@@ -233,24 +175,7 @@ public final class Transform {
      * @param rotation The new rotation
      * @return A new transform
      */
-    public Transform setRotation(Quaterniond rotation) {
-        checkNotNull(rotation, "rotation");
-        return setRotation(toAxesAngles(rotation));
-    }
-
-    /**
-     * Creates a copy of this transform and sets the rotation as a quaternion.
-     *
-     * <p>Quaternions are objectively better than the Euler angles preferred by
-     * Minecraft. This is for compatibility with the flow-math library.</p>
-     *
-     * @param rotation The new rotation
-     * @return A new transform
-     */
-    public Transform setRotation(Vector3d rotation) {
-        checkNotNull(rotation, "rotation");
-        return new Transform(getWorld(), getPosition(), rotation, getScale());
-    }
+    Transform withRotation(Quaterniond rotation);
 
     /**
      * Returns the rotation as a quaternion.
@@ -260,48 +185,35 @@ public final class Transform {
      *
      * @return The rotation
      */
-    public Quaterniond getRotationAsQuaternion() {
-        if (this.rotationQuaternion == null) {
-            this.rotationQuaternion = fromAxesAngles(this.rotation);
-        }
-        return this.rotationQuaternion;
-    }
+    Quaterniond getRotationAsQuaternion();
 
     /**
      * Gets the pitch component of this transform rotation.
      *
      * @return The pitch
      */
-    public double getPitch() {
-        return this.rotation.getX();
-    }
+    double getPitch();
 
     /**
      * Gets the yaw component of this transform rotation.
      *
      * @return The yaw
      */
-    public double getYaw() {
-        return this.rotation.getY();
-    }
+    double getYaw();
 
     /**
      * Gets the roll component of this transform rotation.
      *
      * @return The roll
      */
-    public double getRoll() {
-        return this.rotation.getZ();
-    }
+    double getRoll();
 
     /**
      * Gets the scale of the transform for each axis.
      *
      * @return The scale
      */
-    public Vector3d getScale() {
-        return this.scale;
-    }
+    Vector3d getScale();
 
     /**
      * Creates a copy of this transform and sets the scale for each axis.
@@ -309,10 +221,7 @@ public final class Transform {
      * @param scale The scale
      * @return A new transform
      */
-    public Transform setScale(Vector3d scale) {
-        checkNotNull(scale, "scale");
-        return new Transform(getWorld(), getPosition(), getRotation(), scale);
-    }
+    Transform withScale(Vector3d scale);
 
     /**
      * "Adds" another transform to this one. This is equivalent to adding the
@@ -323,15 +232,7 @@ public final class Transform {
      * @param other The transform to add
      * @return A new transform
      */
-    public Transform add(Transform other) {
-        checkNotNull(other, "other");
-        return new Transform(
-            getWorld(),
-            getPosition().add(other.getPosition()),
-            toAxesAngles(other.getRotationAsQuaternion().mul(getRotationAsQuaternion())),
-            getScale().mul(other.getScale())
-        );
-    }
+    Transform add(Transform other);
 
     /**
      * Adds a translation to this transform.
@@ -341,10 +242,7 @@ public final class Transform {
      * @param translation The translation to add
      * @return A new transform
      */
-    public Transform addTranslation(Vector3d translation) {
-        checkNotNull(translation, "translation");
-        return new Transform(getWorld(), getPosition().add(translation), getRotation(), getScale());
-    }
+    Transform translate(Vector3d translation);
 
     /**
      * Adds a rotation to this transform. Returns the results as a new copy.
@@ -352,10 +250,7 @@ public final class Transform {
      * @param rotation The rotation to add
      * @return A new transform
      */
-    public Transform addRotation(Vector3d rotation) {
-        checkNotNull(rotation, "rotation");
-        return addRotation(fromAxesAngles(rotation));
-    }
+    Transform rotate(Vector3d rotation);
 
     /**
      * Adds a rotation to this transform.
@@ -369,10 +264,7 @@ public final class Transform {
      * @param rotation The rotation to add
      * @return A new transform
      */
-    public Transform addRotation(Quaterniond rotation) {
-        checkNotNull(rotation, "rotation");
-        return new Transform(getWorld(), getPosition(), toAxesAngles(rotation.mul(getRotationAsQuaternion())), getScale());
-    }
+    Transform rotate(Quaterniond rotation);
 
     /**
      * "Adds" a scale to this transform. Scales are multiplicative, so this
@@ -383,10 +275,7 @@ public final class Transform {
      * @param scale The scale to add
      * @return A new transform
      */
-    public Transform addScale(Vector3d scale) {
-        checkNotNull(scale, "scale");
-        return new Transform(getWorld(), getPosition(), getRotation(), getScale().mul(scale));
-    }
+    Transform scale(Vector3d scale);
 
     /**
      * Returns a matrix representation of this transform.
@@ -410,66 +299,9 @@ public final class Transform {
      *
      * @return The transform as a matrix
      */
-    public Matrix4d toMatrix() {
-        return Matrix4d.createScaling(getScale().toVector4(1)).rotate(getRotationAsQuaternion()).translate(getPosition());
-    }
+    Matrix4d toMatrix();
 
-    /**
-     * Returns if this {@link Transform} is still valid.
-     *
-     * <p>Examples of invalid Transforms are:</p>
-     *
-     * <ul>
-     *     <li>A Transform without an {@link World}</li>
-     *     <li>A Transform whose {@link World} object is no longer present</li>
-     *     <li>A Transform whose coordinates are illegal (defined by the
-     *     implementation)</li>
-     * </ul>
-     *
-     * @return True if valid, false if not
-     */
-    public boolean isValid() {
-        return this.world.isLoaded();
+    interface Factory {
+        Transform create(Location location, Vector3d rotation, Vector3d scale);
     }
-
-    @Override
-    public int hashCode() {
-        int result = this.world.hashCode();
-        result = 31 * result + this.position.hashCode();
-        result = 31 * result + this.rotation.hashCode();
-        result = 31 * result + this.scale.hashCode();
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        if (this == other) {
-            return true;
-        }
-        if (!(other instanceof Transform)) {
-            return false;
-        }
-        final Transform otherTransform = (Transform) other;
-        return otherTransform.world.equals(this.world) && otherTransform.getPosition().equals(getPosition())
-            && otherTransform.getRotation().equals(getRotation()) && otherTransform.getScale().equals(getScale());
-    }
-
-    @Override
-    public String toString() {
-        return MoreObjects.toStringHelper(this)
-            .add("location", getLocation())
-            .add("rotation", this.rotation)
-            .add("scale", this.scale)
-            .toString();
-    }
-
-    private static Vector3d toAxesAngles(Quaterniond quaternion) {
-        final Vector3d axesAngles = quaternion.getAxesAnglesDeg();
-        return new Vector3d(axesAngles.getX(), -axesAngles.getY(), axesAngles.getZ());
-    }
-
-    private static Quaterniond fromAxesAngles(Vector3d angles) {
-        return Quaterniond.fromAxesAnglesDeg(angles.getX(), -angles.getY(), angles.getZ());
-    }
-
 }
