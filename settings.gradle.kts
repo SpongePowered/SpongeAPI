406aa19c2ab65d8efe9676962b72585ce4663d5d
Gabriel Harris-Rouquette <gabizou@me.com>
2020-04-11 16:54:09 -0700

    Bump SpongeGradle dependency.
    Signed-off-by: Gabriel Harris-Rouquette <gabizou@me.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,16 @@
 rootProject.name = "SpongeAPI"
 pluginManagement {
     repositories {
-        jcenter()
         mavenLocal()
         mavenCentral()
-        maven("https://repo.spongepowered.org/maven")
         gradlePluginPortal()
+        maven("https://repo.spongepowered.org/maven")
     }
     resolutionStrategy {
         eachPlugin {
             if (requested.id.id.startsWith("org.spongepowered.gradle.")) {
-                val version = requested.version ?: "0.11.0-SNAPSHOT"
-                useModule("org.spongepowered:spongegradle:$version")
+                val version = requested.version ?: "0.11.1"
+                useModule("org.spongepowered:SpongeGradle:$version")
             }
         }
     }
-
-}
