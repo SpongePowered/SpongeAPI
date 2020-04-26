1b868f90832de067f4f2d25f726b74fba20b7976
Gabriel Harris-Rouquette <gabizou@me.com>
2020-04-25 17:52:04 -0700

    Bump SpongeGradle for small updates.
    Signed-off-by: Gabriel Harris-Rouquette <gabizou@me.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,7 @@ pluginManagement {
     resolutionStrategy {
         eachPlugin {
             if (requested.id.id.startsWith("org.spongepowered.gradle.")) {
-                val version = requested.version ?: "0.11.3-SNAPSHOT"
+                val version = requested.version ?: "0.11.3"
                 useModule("org.spongepowered:SpongeGradle:$version")
             }
         }
