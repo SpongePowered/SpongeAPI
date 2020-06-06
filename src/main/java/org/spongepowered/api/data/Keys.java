95b8b0ef45d1619ba65b825c5a02e3855e29fc8c
Anselm Brehme <development.anselm@breh.me>
2020-06-06 11:57:59 +0200

    ArmorType still exists

--- src/main/java/org/spongepowered/api/data/Keys.java
+++ src/main/java/org/spongepowered/api/data/Keys.java
@@ -45,6 +45,7 @@ import org.spongepowered.api.block.entity.carrier.Hopper;
 import org.spongepowered.api.block.entity.carrier.furnace.FurnaceBlockEntity;
 import org.spongepowered.api.boss.ServerBossBar;
 import org.spongepowered.api.data.meta.BannerPatternLayer;
+import org.spongepowered.api.data.type.ArmorType;
 import org.spongepowered.api.data.type.ArtType;
 import org.spongepowered.api.data.type.BodyPart;
 import org.spongepowered.api.data.type.BodyParts;
@@ -304,6 +305,12 @@ public final class Keys {
      * key instead.)</p>
      */
     public static final Supplier<Key<ListValue<Enchantment>>> APPLIED_ENCHANTMENTS = Sponge.getRegistry().getCatalogRegistry().provideSupplier(Key.class, "ITEM_ENCHANTMENTS");
+
+    /**
+     * The {@link ArmorType} of an armor {@link ItemStack}.
+     * Readonly
+     */
+    public static final Supplier<Key<Value<ArmorType>>> ARMOR_TYPE = Sponge.getRegistry().getCatalogRegistry().provideSupplier(Key.class, "ARMOR");
 
     /**
      * The type of {@link ArtType} shown by {@link Painting}s.
---
+++ src/main/java/org/spongepowered/api/data/type/ArmorTypes.java
@@ -0,0 +1,52 @@
+/*
+ * This file is part of SpongeAPI, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.api.data.type;
+
+import org.spongepowered.api.util.generator.dummy.DummyObjectProvider;
+
+public final class ArmorTypes {
+
+    // SORTFIELDS:ON
+
+    public static final ArmorType CHAINMAIL = DummyObjectProvider.createFor(ArmorType.class, "CHAINMAIL");
+
+    public static final ArmorType DIAMOND = DummyObjectProvider.createFor(ArmorType.class, "DIAMOND");
+
+    public static final ArmorType GOLD = DummyObjectProvider.createFor(ArmorType.class, "GOLD");
+
+    public static final ArmorType IRON = DummyObjectProvider.createFor(ArmorType.class, "IRON");
+
+    public static final ArmorType LEATHER = DummyObjectProvider.createFor(ArmorType.class, "LEATHER");
+
+    public static final ArmorType TURTLE = DummyObjectProvider.createFor(ArmorType.class, "TURTLE");
+
+    // SORTFIELDS:OFF
+
+    // Suppress default constructor to ensure non-instantiability.
+    private ArmorTypes() {
+        throw new AssertionError("You should not be attempting to instantiate this class.");
+    }
+
+}
