71a5e3d42dfd416f1e21a7a4d688206c789de96a
Daniel Naylor <git@drnaylor.co.uk>
2020-07-08 23:43:29 +0100

    Add notion of a "ClientCompletionType" to enable some specific completion hints to the client

--- src/main/java/org/spongepowered/api/command/parameter/managed/ValueParser.java
+++ src/main/java/org/spongepowered/api/command/parameter/managed/ValueParser.java
@@ -24,14 +24,19 @@
  */
 package org.spongepowered.api.command.parameter.managed;
 
+import com.google.common.collect.ImmutableList;
 import org.spongepowered.api.command.CommandExecutor;
 import org.spongepowered.api.command.exception.ArgumentParseException;
 import org.spongepowered.api.command.parameter.ArgumentReader;
 import org.spongepowered.api.command.parameter.CommandContext;
 import org.spongepowered.api.command.parameter.Parameter;
+import org.spongepowered.api.command.parameter.managed.clientcompletion.ClientCompletionType;
+import org.spongepowered.api.command.parameter.managed.clientcompletion.ClientCompletionTypes;
 import org.spongepowered.api.event.cause.Cause;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.function.Supplier;
 
 /**
  * Defines how a parameter should be parsed.
@@ -63,9 +68,12 @@ public interface ValueParser<T> {
      * <p>The {@link Cause} of this parse is provided in the
      * {@link CommandContext.Builder}.</p>
      *
-     * @param parameterKey The {@link Parameter.Key} of the parameter being parsed
-     * @param reader The {@link ArgumentReader} that contains the unparsed arguments
-     * @param context The {@link CommandContext} containing the state about this command
+     * @param parameterKey The {@link Parameter.Key} of the parameter being
+     *          parsed
+     * @param reader The {@link ArgumentReader} that contains the unparsed
+     *          arguments
+     * @param context The {@link CommandContext} containing the state about this
+     *          command
      * @return Returns the value(s)
      * @throws ArgumentParseException if a parameter could not be parsed
      */
@@ -73,5 +81,37 @@ public interface ValueParser<T> {
             Parameter.Key<? super T> parameterKey,
             ArgumentReader.Mutable reader,
             CommandContext.Builder context) throws ArgumentParseException;
+
+    /**
+     * Provides a hint to the client completion engine what sort of input(s)
+     * the client should expect.
+     *
+     * <p>Multiple elements can be placed here for multi part parsers - that
+     * is, parsers that call parse methods on the {@link ArgumentReader.Mutable}
+     * more than once. However, it is recommended that you consider splitting
+     * your parsers up to consume one element at any time, any completions you
+     * provide <strong>may not be provided to the client</strong> if you do so.
+     * </p>
+     *
+     * <p>Returning an empty list <strong>does not</strong> infer that the
+     * element will not be displayed, rather, an empty list infers that the
+     * command system will decide. Custom parsers will default to a single
+     * {@link ClientCompletionTypes#STRING}. Be aware, providing a
+     * non-string type here <em>may</em> cause your completions to be
+     * ignored.</p>
+     *
+     * <p>If you wish to hide this element from client completion completely,
+     * return a {@link List} with a single {@link ClientCompletionTypes#NONE}.
+     * </p>
+     *
+     * <p>Remember that anything supplied here is a hint. It is possible
+     * that this hint will be ignored in some scenarios, particularly if
+     * this parser is one of many supplied to {@link Parameter.Value}.</p>
+     *
+     * @return The {@link ClientCompletionType}s to use on the client.
+     */
+    default List<ClientCompletionType> getClientCompletionType() {
+        return ImmutableList.of();
+    }
 
 }
---
+++ src/main/java/org/spongepowered/api/command/parameter/managed/clientcompletion/ClientCompletionTypes.java
@@ -0,0 +1,81 @@
+/*
+ * This file is part of SpongeAPI, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.api.command.parameter.managed.clientcompletion;
+
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.parameter.managed.ValueParser;
+
+import java.util.function.Supplier;
+
+/**
+ * Supported {@link ClientCompletionTypes}.
+ */
+public final class ClientCompletionTypes {
+
+    // SORTFIELDS: ON
+
+    /**
+     * Indicates to the client that the {@link ValueParser} will parse a
+     * decimal number.
+     */
+    public static final Supplier<ClientCompletionType> DECIMAL_NUMBER =
+            Sponge.getRegistry().getCatalogRegistry().provideSupplier(ClientCompletionType.class, "DECIMAL_NUMBER");
+
+    /**
+     * Indicates to the client that the {@link ValueParser} will parse Json.
+     */
+    public static final Supplier<ClientCompletionType> JSON =
+            Sponge.getRegistry().getCatalogRegistry().provideSupplier(ClientCompletionType.class, "JSON");
+
+    /**
+     * Hides this element from the client - useful for if the
+     * {@link ValueParser} does not parse any part of the string.
+     */
+    public static final Supplier<ClientCompletionType> NONE =
+            Sponge.getRegistry().getCatalogRegistry().provideSupplier(ClientCompletionType.class, "NONE");
+
+    /**
+     * Indicates to the client that the {@link ValueParser} is a standard
+     * string.
+     *
+     * <p>This is the default behaviour for any custom {@link ValueParser}.</p>
+     */
+    public static final Supplier<ClientCompletionType> STRING =
+            Sponge.getRegistry().getCatalogRegistry().provideSupplier(ClientCompletionType.class, "STRING");
+
+    /**
+     * Indicates to the client that the {@link ValueParser} accepts a whole
+     * number.
+     */
+    public static final Supplier<ClientCompletionType> WHOLE_NUMBER =
+            Sponge.getRegistry().getCatalogRegistry().provideSupplier(ClientCompletionType.class, "WHOLE_NUMBER");
+
+    // SORTFIELDS: OFF
+
+    private ClientCompletionTypes() {
+        throw new IllegalStateException("This should not be callable");
+    }
+
+}
