0188979bf641a05c7487a333b5ba936f5dbf5113
Phillip Schichtel <phillip@schich.tel>
2021-02-13 18:12:59 +0100

    Simplified the metrics interface
    Instead of registering individual collectors, users have to add entire registries. This allows to more closely follow the usual pattern of creating your collectors once statically, that is usually applied.
    
    Additionally I documented the interface somewhat, especially around what is expected by implementations.
    
    Signed-off-by: Phillip Schichtel <phillip@schich.tel>

--- src/main/java/org/spongepowered/api/service/metrics/MetricsService.java
+++ src/main/java/org/spongepowered/api/service/metrics/MetricsService.java
@@ -24,13 +24,40 @@
  */
 package org.spongepowered.api.service.metrics;
 
-import io.prometheus.client.Collector;
+import io.prometheus.client.CollectorRegistry;
 import org.spongepowered.plugin.PluginContainer;
 
+/**
+ * The service can be used to register additional {@link CollectorRegistry} instances for scraping by prometheus.
+ *
+ * Implementations can chose to scrape Prometheus' global default registry, however plugins should register their own registry. No name or label rules are enforced, however it is advisable
+ * to introduce a plugin specific prefix to plugin specific metrics.
+ */
 public interface MetricsService {
-    void registerCollector(PluginContainer plugin, SyncCollector collector);
-    void registerCollector(PluginContainer plugin, Collector collector);
-    void unregisterCollector(PluginContainer plugin, SyncCollector collector);
-    void unregisterCollector(PluginContainer plugin, Collector collector);
-    void unregisterCollectors(PluginContainer plugin);
+    /**
+     * Registers an additional {@link CollectorRegistry} to be scraped. Time series returned by the {@link io.prometheus.client.Collector} will be namespaced by the plugin to prevent name clashes.
+     * The standard Prometheus collectors ({@link io.prometheus.client.Gauge}, {@link io.prometheus.client.Counter}, ...) are thread safe and thus safe for
+     * scraping from any thread. If you use custom collector implementations or make use of the provided {@link org.spongepowered.api.service.metrics.pullgauge.PullGauge} interface, make sure your
+     * implementation is safe to be called from any thread or add your registry with the asyncCapable parameter set to false.
+     *
+     * @param plugin the owning plugin
+     * @param registry the registry to be scraped
+     * @param asyncCapable whether the collectors in the given registry can by scraped outside of the server main thread
+     */
+    void addCollectorRegistry(PluginContainer plugin, CollectorRegistry registry, boolean asyncCapable);
+
+    /**
+     * Removes the given {@link CollectorRegistry} from scraping.
+     *
+     * @param plugin the owning plugin
+     * @param registry the registry to be removed from scraping
+     */
+    void removeCollectorRegistry(PluginContainer plugin, CollectorRegistry registry);
+
+    /**
+     * Removes all {@link CollectorRegistry} instances owned by the given plugin from scaping.
+     *
+     * @param plugin the owning plugin
+     */
+    void removeAllCollectorRegistries(PluginContainer plugin);
 }
