e3a6fa6b0f95b267cdff28c342910b47b1065b9e
Phillip Schichtel <phillip@schich.tel>
2021-02-14 21:40:50 +0100

    Drop TracingService
    Signed-off-by: Phillip Schichtel <phillip@schich.tel>

--- build.gradle.kts
+++ build.gradle.kts
@@ -129,7 +129,6 @@ dependencies {
 
     // Observability
     api("io.prometheus:simpleclient:0.10.0")
-    api("io.opentracing:opentracing-api:0.33.0")
 
     testImplementation(platform("org.junit:junit-bom:5.7.1"))
     testImplementation("org.junit.jupiter:junit-jupiter-api")
