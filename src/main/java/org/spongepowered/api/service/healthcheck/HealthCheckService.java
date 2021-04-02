a0a9a1d146c94511bc74d03bc9b194936d42290c
Phillip Schichtel <phillip@schich.tel>
2021-04-02 21:13:53 +0200

    fix checkstyle errors in HealthCheckService

--- src/main/java/org/spongepowered/api/service/healthcheck/HealthCheckService.java
+++ src/main/java/org/spongepowered/api/service/healthcheck/HealthCheckService.java
@@ -28,7 +28,10 @@ import org.spongepowered.plugin.PluginContainer;
 
 public interface HealthCheckService {
     void registerProbe(PluginContainer plugin, String id, SyncHealthProbe probe);
+
     void registerProbe(PluginContainer plugin, String id, AsyncHealthProbe probe);
+
     void unregisterProbe(PluginContainer plugin, String id);
+
     void unregisterProbes(PluginContainer plugin);
 }
