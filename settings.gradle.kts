e14c171409f688ac82557f9b3b01a5932170386d
zml <zml@stellardrift.ca>
2021-08-02 17:13:17 -0700

    build: Bump to Gradle 7.2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,9 +7,9 @@ pluginManagement {
         }
     }
     plugins {
-        val indraVersion = "2.0.5"
-        id("org.spongepowered.gradle.sponge.dev") version "1.1.0"
-        id("org.cadixdev.licenser") version "0.6.0"
+        val indraVersion = "2.0.6"
+        id("org.spongepowered.gradle.sponge.dev") version "1.1.1"
+        id("org.cadixdev.licenser") version "0.6.1"
         id("org.jetbrains.gradle.plugin.idea-ext") version "1.0"
         id("net.ltgt.errorprone") version "2.0.1"
         id("net.kyori.indra.publishing") version indraVersion
