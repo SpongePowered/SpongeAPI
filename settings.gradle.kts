7e6c50f6d5248bc9326a45b38d935d7bc34aa23e
zml <zml@stellardrift.ca>
2022-12-12 19:28:12 -0800

    chore(build): upgradle to 7.6

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,11 +10,15 @@ pluginManagement {
         val indraVersion = "3.0.1"
         id("org.spongepowered.gradle.event-impl-gen") version "7.0.0"
         id("org.spongepowered.gradle.sponge.dev") version "2.1.1"
-        id("org.jetbrains.gradle.plugin.idea-ext") version "1.1.6"
+        id("org.jetbrains.gradle.plugin.idea-ext") version "1.1.7"
         id("net.ltgt.errorprone") version "3.0.1"
         id("net.kyori.indra.publishing") version indraVersion
         id("net.kyori.indra.publishing.sonatype") version indraVersion
         id("net.kyori.indra.checkstyle") version indraVersion
         id("net.kyori.indra.crossdoc") version indraVersion
     }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.3.0")
 }
