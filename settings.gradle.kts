25fc6192f4f19f9f4e5cd1189008cfe3205c7d1e
Morpheus <ImMorpheus@users.noreply.github.com>
2023-04-01 22:10:56 +0000

    build: require java 17+ and update readme (#2448)
    * build: require java 17+ and update readme
    
    * ci: update java version in github workflows
    
    * ci: update java version in github workflows pt 2
    
    * Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,7 +21,3 @@ pluginManagement {
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version ("0.3.0")
 }
-
-if (JavaVersion.current() < JavaVersion.VERSION_11) {
-    throw GradleException("SpongeAPI requires at least Java 11 to build, but you have ${JavaVersion.current()}.")
-}
