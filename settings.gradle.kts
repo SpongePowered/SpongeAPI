9bd831a8c7e9f208e2af018d9ce593d7fbb5056a
zml <zml@stellardrift.ca>
2024-01-23 14:50:15 -0800

    chore(build): Move to using version catalogs for dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,18 +6,8 @@ pluginManagement {
             name = "sponge"
         }
     }
-    plugins {
-        val indraVersion = "3.1.3"
-        id("org.spongepowered.gradle.event-impl-gen") version "7.0.0"
-        id("org.spongepowered.gradle.sponge.dev") version "2.2.0"
-        id("org.jetbrains.gradle.plugin.idea-ext") version "1.1.7"
-        id("net.ltgt.errorprone") version "3.1.0"
-        id("net.kyori.indra.publishing") version indraVersion
-        id("net.kyori.indra.publishing.sonatype") version indraVersion
-        id("net.kyori.indra.checkstyle") version indraVersion
-        id("net.kyori.indra.crossdoc") version indraVersion
-    }
 }
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version ("0.7.0")
 }
