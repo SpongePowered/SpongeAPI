9d3c8f679629f3e0d41fd8ceffa20b6688abc75b
Morpheus <ImMorpheus@users.noreply.github.com>
2024-06-30 13:08:57 +0000

    Update Llama.java

--- src/main/java/org/spongepowered/api/entity/living/animal/horse/llama/Llama.java
+++ src/main/java/org/spongepowered/api/entity/living/animal/horse/llama/Llama.java
@@ -27,8 +27,8 @@ package org.spongepowered.api.entity.living.animal.horse.llama;
 import org.spongepowered.api.data.Keys;
 import org.spongepowered.api.data.type.LlamaType;
 import org.spongepowered.api.data.value.Value;
-import org.spongepowered.api.entity.living.animal.horse.PackHorse;
 import org.spongepowered.api.entity.living.Ranger;
+import org.spongepowered.api.entity.living.animal.horse.PackHorse;
 
 /**
  * Represents a Llama. Llamas are unique in that
