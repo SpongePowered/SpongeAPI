0450e3a1d78ccca15b4336f915a80a64ccf6500e
Morpheus <ImMorpheus@users.noreply.github.com>
2024-06-30 11:52:42 +0000

    Update slime parent interface (#2539)
    * Update slime parent interface
    
    * Update Slime.java

--- src/main/java/org/spongepowered/api/entity/living/slime/Slime.java
+++ src/main/java/org/spongepowered/api/entity/living/slime/Slime.java
@@ -22,11 +22,25 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package org.spongepowered.api.entity.living.monster.slime;
+package org.spongepowered.api.entity.living.slime;
+
+import org.spongepowered.api.data.Keys;
+import org.spongepowered.api.data.value.Value;
+import org.spongepowered.api.entity.living.Agent;
+import org.spongepowered.api.entity.living.Hostile;
 
 /**
- * Represents a Magma Cube.
+ * Represents a Slime.
  */
-public interface MagmaCube extends Slime {
+public interface Slime extends Agent, Hostile {
+
+    /**
+     * {@link Keys#SIZE}
+     *
+     * @return The size of slime
+     */
+    default Value.Mutable<Integer> size() {
+        return this.requireValue(Keys.SIZE).asMutable();
+    }
 
 }
